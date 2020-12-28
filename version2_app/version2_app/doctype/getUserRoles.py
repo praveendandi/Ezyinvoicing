8bee0044f0268c78260477c0ff2237840eb6228b
kiran <kiran@caratred.com>
2020-12-28 16:19:12 +0530

    added user roles update

--- version2_app/version2_app/doctype/getUserRoles.py
+++ version2_app/version2_app/doctype/getUserRoles.py
@@ -1,15 +1,45 @@
 import frappe
 import json
 import requests
+
+
 @frappe.whitelist(allow_guest=True)
 def getUserRoles():
-    if frappe.local.request.method=="GET":
-        doc = frappe.get_roles(frappe.session.user)
-        return {"success":True,"data":doc}
-    else:
-        return {"success":False, "message":"User doesn't exists! please Register"}
-
-
-
-
-    
+	if frappe.local.request.method == "POST":
+		data = json.loads(frappe.request.data)
+		print(data,"etst")
+		for i in data['role']:
+			doc = frappe.get_doc({
+				"docstatus": 0,
+				"doctype": "Has Role",
+				"modified_by": "Administrator",
+				"owner": "Administrator",
+				"parent": data['parent'],
+				'parentfield': "roles",
+				"parenttype": "User",
+				"role": i,
+				})
+			doc.save()
+			frappe.db.commit()
+			
+		return {"success":True}
+	if frappe.local.request.method=="PUT":
+		data = json.loads(frappe.request.data)
+		par = frappe.db.delete('Has Role', {'parent': data['parent']})
+		# frappe.db.commit()
+		for i in data['role']:
+			doc = frappe.get_doc({
+				"docstatus": 0,
+				"doctype": "Has Role",
+				 # "idx": 1,
+				"modified_by": "Administrator",
+				"owner": "Administrator",
+				"parent": data['parent'],
+				'parentfield': "roles",
+				"parenttype": "User",
+				"role": i,
+				})
+			doc.save()
+			frappe.db.commit()
+		return {"success":True}
+		
