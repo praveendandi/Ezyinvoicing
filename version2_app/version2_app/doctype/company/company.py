15e2d32962b018c962fbf40e0cd885f6edce126a
prasanthvajja <prasanth@caratred.com>
2021-01-13 12:58:27 +0530

    changed the b2c status script

--- version2_app/version2_app/doctype/company/company.py
+++ version2_app/version2_app/doctype/company/company.py
@@ -10,7 +10,6 @@ import shutil
 import os
 import pdfplumber
 from datetime import date, datetime
-import mysql.connector as mysql
 import requests
 import pandas as pd
 import re
@@ -122,36 +121,37 @@ def gitCurrentBranchCommit():
 @frappe.whitelist(allow_guest=True)
 def b2cstatusupdate():
 	try:
-		db = mysql.connect(
-			host = "127.0.0.1",
-			user = "root",
-			passwd = "root",
-			database = "_ae17eaceb43e7f9c"
-		)
-		cursor = db.cursor()
-		query = "SELECT name,invoice_number,qr_generated,irn_generated,b2c_qrimage FROM tabInvoices where invoice_type='B2C';"
-		cursor.execute(query)
-		records = cursor.fetchall()
-		for each in records:
-			if each[3] == "NA" and each[2] == "Pending":
-				print(1)
-				update = "Update tabInvoices set qr_generated = 'Success' where name = '"+each[0]+"';"
-			elif each[3] == "Zero Invoice" and each[2] == "Pending":
-				print(2)
-				update = "Update tabInvoices set qr_generated = 'Zero Invoice', irn_generated = 'NA' where name = '"+each[0]+"';"
-			elif each[3] == "Error" and each[2] == "Pending":
-				print(2)
-				update = "Update tabInvoices set qr_generated = 'Error', irn_generated = 'NA' where name = '"+each[0]+"';"
-			elif each[3] == "Error" and each[2] == "Success":
-				print(4)
-				update = "Update tabInvoices set irn_generated = 'NA' where name = '"+each[0]+"';"
-			elif each[3] == "Zero Invoice" and each[2] == "Success":
-				update = "Update tabInvoices set qr_generated = 'Zero Invoice', irn_generated = 'NA' where name = '"+each[0]+"';"
+		data = frappe.db.get_list('Invoices',filters={'invoice_type': 'B2C'},fields=["name","invoice_number","qr_generated","irn_generated","b2c_qrimage"])
+		for each in data:
+			print(each)
+			if each["irn_generated"] == "NA" and each["qr_generated"] == "Pending":
+				doc = frappe.get_doc("Invoices",each["name"])
+				doc.qr_generated = "Success"
+			elif each["irn_generated"] == "Zero Invoice" and each["qr_generated"] == "Pending":
+				doc = frappe.get_doc("Invoices",each["name"])
+				doc.qr_generated = "Zero Invoice"
+				doc.irn_generated = "NA"
+				# update = "Update tabInvoices set qr_generated = 'Zero Invoice', irn_generated = 'NA' where name = '"+each[0]+"';"
+			elif each["irn_generated"] == "Error" and each["qr_generated"] == "Pending":
+				doc = frappe.get_doc("Invoices",each["name"])
+				doc.qr_generated = "Error"
+				doc.irn_generated = "NA"
+				# update = "Update tabInvoices set qr_generated = 'Error', irn_generated = 'NA' where name = '"+each[0]+"';"
+			elif each["irn_generated"] == "Error" and each["qr_generated"] == "Success":
+				doc = frappe.get_doc("Invoices",each["name"])
+				doc.irn_generated = "NA"
+				# update = "Update tabInvoices set irn_generated = 'NA' where name = '"+each[0]+"';"
+			elif each["irn_generated"] == "Zero Invoice" and each["qr_generated"] == "Success":
+				doc = frappe.get_doc("Invoices",each["name"])
+				doc.qr_generated = "Zero Invoice"
+				doc.irn_generated = "NA"
+				# update = "Update tabInvoices set qr_generated = 'Zero Invoice', irn_generated = 'NA' where name = '"+each[0]+"';"
 			else:
-				update = ""
-			cursor.execute(update)
-			commit_changes = db.commit()
-			print(commit_changes)
+				doc = None
+			if doc == None:
+				continue
+			doc.save()
+			frappe.db.commit()
 		return True
 	except Exception as e:
 		print("b2cstatusupdate", str(e))
