e37f28ad5b4033ae3391ed5b9eb90a7b80719112
caratRED <ganesh@caratred.com>
2021-01-18 18:50:37 +0530

    remove s in irn generation

--- version2_app/version2_app/doctype/invoices/invoices.py
+++ version2_app/version2_app/doctype/invoices/invoices.py
@@ -262,7 +262,7 @@ class Invoices(Document):
 								invoice.irn_number = response['result'][0]['Desc']['Irn']
 								invoice.ack_date = response['result'][0]['Desc']['AckDt']
 								invoice.irn_generated = "Success"
-								invoices.qr_code_generated = "Success"
+								invoice.qr_code_generated = "Success"
 								invoice.save(ignore_permissions=True, ignore_version=True)
 							
 							irn_error_message = response["message"]
