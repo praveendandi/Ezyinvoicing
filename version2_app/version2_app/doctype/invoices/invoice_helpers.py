dfe77dcfa4bef46d602b38a8ae5dbd8d23a9487b
caratRED <ganesh@caratred.com>
2021-02-10 14:26:30 +0530

    total_invoice_amount in reinit

--- version2_app/version2_app/doctype/invoices/invoice_helpers.py
+++ version2_app/version2_app/doctype/invoices/invoice_helpers.py
@@ -148,7 +148,7 @@ def TotalMismatchError(data,calculated_data):
 
 def CheckRatePercentages(data, sez, placeofsupply, exempted, state_code):
     try:
-        if data['item_value']>1000 and data['item_value']<=7500:
+        if data['item_value']>=1000 and data['item_value']<=7500:
             gst_percentage = 12
         elif data['item_value'] > 7500:
             gst_percentage = 18
