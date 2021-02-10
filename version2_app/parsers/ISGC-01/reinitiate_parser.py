751672018a239817d63695e71e43f5d226834858
caratRED <ganesh@caratred.com>
2021-02-10 15:41:51 +0530

    ammend invoice search like

--- version2_app/parsers/ISGC-01/reinitiate_parser.py
+++ version2_app/parsers/ISGC-01/reinitiate_parser.py
@@ -104,7 +104,10 @@ def reinitiateInvoice(data):
 			if "Printed By / On" in i:
 				p = i.split(":")
 				print_by = p[1].replace(" ","")
-
+		check_invoice = check_invoice_exists(invoiceNumber)
+		if check_invoice['success']==True:
+			inv_data = check_invoice['data']
+			invoiceNumber = inv_data.name
 		if invoiceNumber != reupload_inv_number:
 			return {"success":False,"message":"Incorrect Invoice Attempted"}
 		items = [] 
