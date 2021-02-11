c1c6fd34bd1b5b70526bf9ca370d419aa7ede4c7
caratRED <ganesh@caratred.com>
2021-02-11 10:02:05 +0530

    ammened process

--- version2_app/parsers/ISGC-01/invoice_parser.py
+++ version2_app/parsers/ISGC-01/invoice_parser.py
@@ -187,8 +187,9 @@ def file_parsing(filepath):
 			# print(inv_data.__dict__)
 			if inv_data.docstatus==2:
 				amened='Yes'
+				invoiceNumber = inv_data.name
+				guest['invoice_number'] = inv_data.name
 			else:
-				print(inv_data.name,"////////")
 				invoiceNumber = inv_data.name
 				guest['invoice_number'] = inv_data.name
 				amened='No'
