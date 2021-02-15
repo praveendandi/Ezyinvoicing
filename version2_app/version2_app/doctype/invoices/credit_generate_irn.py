daa3c9b973734c0d1da66200ffec7b853a5a10e6
prasanthvajja <prasanth@caratred.com>
2021-02-15 17:08:55 +0530

    updated cess in credit note

--- version2_app/version2_app/doctype/invoices/credit_generate_irn.py
+++ version2_app/version2_app/doctype/invoices/credit_generate_irn.py
@@ -455,6 +455,7 @@ def CreditgenerateIrn(invoice_number):
 		total_sgst_value = 0
 		total_cgst_value = 0
 		total_cess_calue = 0
+		total_state_cess_value
 		ass_value = 0
 		for index, item in enumerate(invoice.items):
 			# print(item.sac_code,"HsnCD")
@@ -464,6 +465,7 @@ def CreditgenerateIrn(invoice_number):
 				total_sgst_value += abs(item.sgst_amount)
 				total_cgst_value += abs(item.cgst_amount)
 				total_cess_calue += abs(item.cess_amount)
+				total_state_cess_value += abs(item.state_cess_amount)
 				ass_value += abs(item.item_value)
 				i = {
 					"SlNo":
@@ -498,13 +500,13 @@ def CreditgenerateIrn(invoice_number):
 					"CesRt":
 					item.cess,
 					"CesAmt":
-					item.cess_amount,
+					abs(item.cess_amount),
 					"CesNonAdvlAmt":
 					0,
 					"StateCesRt":
-					0,
+					item.state_cess,
 					"StateCesAmt":
-					0,
+					abs(item.state_cess_amount),
 					"StateCesNonAdvlAmt":
 					0,
 					"OthChrg":
