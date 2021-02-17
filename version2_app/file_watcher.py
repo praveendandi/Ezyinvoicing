ac6c25be93a8f03a16b3490cd6467b721f370058
prasanthvajja <prasanth@caratred.com>
2021-02-17 09:44:05 +0530

    code modified

--- version2_app/file_watcher.py
+++ version2_app/file_watcher.py
@@ -1,32 +1,36 @@
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
-import os
+import os,sys,time
 import frappe
 
 abs_path = os.path.dirname(os.getcwd())
 company = frappe.db.get_list('company',['name','ipp_port','folios_folder_path'])
-print(company)
 folder_path = abs_path+company[0]["folios_folder_path"]+company[0]["name"]
 class MyHandler(PatternMatchingEventHandler):
     patterns=["*.pdf"]
     # This function is for data parsing 
     def process(self, event):
-        print("=========================")
-    def on_created(self, event):
+        file_path = event.src_path
+        payload = {
+            'is_private': 1,
+            'folder': 'Home',
+            'doctype': 'invoices',
+            'docname': data["company"],
+            'fieldname': 'invoice'}
+        file_response = requests.post(data['host']+"upload_file",
+                                files=files, data=payload, verify=False).json()
+    def on_created(self, event):        
         self.process(event)
 
 
+args = sys.argv[1:]
+observer = Observer()
+observer.schedule(MyHandler(), path=folder_path)
+observer.start()
 
-if __name__ == '__main__':
-    my_logger.info("Started")
-    args = sys.argv[1:]
-    observer = Observer()
-    observer.schedule(MyHandler(), path=folder_path)
-    observer.start()
-
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
--- version2_app/ipp_printer.py
+++ version2_app/ipp_printer.py
@@ -2,7 +2,6 @@ import asyncio
 import os,shlex
 import frappe
 from subprocess import Popen, PIPE, STDOUT
-from version2_app.ipp_server_master.ippserver.__main__ import *
 def safe_decode(string, encoding='utf-8'):
     try:
         string = string.decode(encoding)
@@ -12,18 +11,13 @@ def safe_decode(string, encoding='utf-8'):
 
 def main():
     abs_path = os.path.dirname(os.getcwd())
-    print("============================================================")
     company = frappe.db.get_list('company',['name','ipp_port','folios_folder_path'])
     if len(company)>0:
         folder_path = abs_path+company[0]["folios_folder_path"]+company[0]["name"]
         if not os.path.isdir(folder_path):
             os.mkdir(folder_path)
-        # test = os.popen("python -m ippserver --port 3000 save --pdf /home/caratred/Downloads/tmp/").read()
-        commands = "python -m ippserver --port "+company[0]["ipp_port"]+" save --pdf "+folder_path
-        # print(commands,"=====================================")
+        commands = "python -m ippserver --host 0.0.0.0 --port "+company[0]["ipp_port"]+" save --pdf "+folder_path
         terminal = Popen(shlex.split(commands), stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-        print(terminal,"=====================================")
         output = safe_decode(terminal.stdout.read(1))
-        print(output)
         
 main()
