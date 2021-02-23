b776b9d6e0734203e56262c05d92f899c9edf27e
caratRED <ganesh@caratred.com>
2021-02-23 17:26:25 +0530

    debit credit note

--- version2_app/version2_app/report/debit_credit_note/debit_credit_note.js
+++ version2_app/version2_app/report/debit_credit_note/debit_credit_note.js
@@ -2,8 +2,23 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["Debit Credit Note"] = {
-	"filters": [
+frappe.query_reports["Debit Credit Note"] = 
+{
+    "filters": [
 
-	]
+        {
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "width": "80",
+            "default": frappe.datetime.month_start()
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "width": "80",
+            "default": frappe.datetime.month_end()
+        }
+    ]
 };
