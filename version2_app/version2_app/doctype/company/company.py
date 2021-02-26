6ba0cf2531c4197e6d624bf57c9c8eec164c1b49
caratRED <ganesh@caratred.com>
2021-02-26 15:25:53 +0530

    delete keys in company

--- version2_app/version2_app/doctype/company/company.py
+++ version2_app/version2_app/doctype/company/company.py
@@ -427,8 +427,7 @@ def run_command(commands,
     finally:
         print("finalyyyyyyy")
         frappe.db.commit()
-        # hack: frappe.db.commit() to make sure the log created is robust,
-        # and the _refresh throws an error if the doc is deleted
+        
         _refresh(doctype=doctype, docname=docname, commands=commands)
 @frappe.whitelist(allow_guest=True)
 def bench_migrate():
