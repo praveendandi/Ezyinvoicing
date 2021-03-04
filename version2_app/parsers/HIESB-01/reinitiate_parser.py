a35e931f03119a192846793b5f41a505f1a8c9b9
prasanthvajja <prasanth@caratred.com>
2021-03-04 19:40:01 +0530

    modified Hoilday in omr parsers

--- version2_app/parsers/HIESB-01/reinitiate_parser.py
+++ version2_app/parsers/HIESB-01/reinitiate_parser.py
@@ -114,6 +114,7 @@ def reinitiateInvoice(data):
         items = [] 
         itemsort = 0
         for i in data:
+            i = i.strip()
             pattern = re.compile(
             "^([0-9]{4}|[0-9]{2})[./-]([0]?[1-9]|[1][0-2])[./-]([0]?[1-9]|[1|2][0-9]|[3][0|1])+"
             )
