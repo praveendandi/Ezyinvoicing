3a477b7885e69871e14e887b8a89126f1028ccd1
prasanthvajja <prasanth@caratred.com>
2021-03-08 12:44:03 +0530

    modified HIBR-01 parsers

--- version2_app/parsers/HIBR-01/reinitiate_parser.py
+++ version2_app/parsers/HIBR-01/reinitiate_parser.py
@@ -146,7 +146,7 @@ def reinitiateInvoice(data):
 		paymentTypes = GetPaymentTypes()
 		payment_Types  = [''.join(each) for each in paymentTypes['data']]
 		for each in items:
-			if "CGST" not in each["name"] and "SGST" not in each["name"] and "CESS" not in each["name"] and "VAT" not in each["name"] and "Cess" not in each["name"] and "Vat" not in each["name"] and "IGST" not in each["name"] and "Service Charge" not in each["name"]:
+			if "CGST" not in each["name"] and "SGST" not in each["name"] and "CESS" not in each["name"] and "Cess" not in each["name"] and "IGST" not in each["name"] and "Service Charge" not in each["name"]:
 				if each["name"] not in payment_Types:
 					total_items.append(each)
 
