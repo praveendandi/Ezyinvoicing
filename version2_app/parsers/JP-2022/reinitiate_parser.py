9ca4a5aa549632394813071bb65d42e5bac5018d
prasanthvajja <prasanth@caratred.com>
2021-03-09 14:24:51 +0530

    updated jps parsers

--- version2_app/parsers/JP-2022/reinitiate_parser.py
+++ version2_app/parsers/JP-2022/reinitiate_parser.py
@@ -117,7 +117,7 @@ def reinitiateInvoice(data):
 			"^([0]?[1-9]|[1|2][0-9]|[3][0|1])[./-]([0]?[1-9]|[1][0-2])[./-]([0-9]{4}|[0-9]{2})+"
 			)
 			check_date = re.findall(pattern, i)
-			if len(check_date) > 0:
+			if len(check_date) > 0 and "XXXXX" not in i:
 				item = dict()
 				item_value = ""
 				dt = i.strip()
