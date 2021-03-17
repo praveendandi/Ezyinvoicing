33c0b5b5410a30684473023cce8c16f48320c2ff
Arvind kumar <arvind@caratred.com>
2021-03-17 12:21:36 +0530

    updated lalith parsers

--- version2_app/parsers/TLND-01/reinitiate_parser.py
+++ version2_app/parsers/TLND-01/reinitiate_parser.py
@@ -116,6 +116,7 @@ def reinitiateInvoice(data):
 		items = [] 
 		itemsort = 0
 		for i in data:
+			i = i.strip()
 			pattern = re.compile(
 			"^([0]?[1-9]|[1|2][0-9]|[3][0|1])[./-]([0]?[1-9]|[1][0-2])[./-]([0-9]{4}|[0-9]{2})+"
 			)
