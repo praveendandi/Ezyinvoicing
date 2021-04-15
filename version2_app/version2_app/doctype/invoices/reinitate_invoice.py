c26750273d1c9397798c458c35b245c71bb2170b
prasanthvajja <prasanth@caratred.com>
2021-04-15 11:03:06 +0530

    issue in reinitiate_invoice

--- version2_app/version2_app/doctype/invoices/reinitate_invoice.py
+++ version2_app/version2_app/doctype/invoices/reinitate_invoice.py
@@ -6,18 +6,13 @@ import datetime
 import json
 import random
 import traceback
-import string
+import string,math
 from frappe.utils import get_site_name
 import time
 from version2_app.version2_app.doctype.invoices.invoice_helpers import TotalMismatchError, CheckRatePercentages
 from version2_app.version2_app.doctype.invoices.invoices import insert_items,insert_hsn_code_based_taxes,send_invoicedata_to_gcb,TaxSummariesInsert,generateIrn
 from version2_app.version2_app.doctype.invoices.invoice_helpers import CheckRatePercentages
 from PyPDF2 import PdfFileWriter, PdfFileReader
-import math
-
-
-
-
 
 @frappe.whitelist()
 def Reinitiate_invoice(data):
