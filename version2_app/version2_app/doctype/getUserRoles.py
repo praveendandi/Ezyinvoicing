e1a9fd329f6bfaea931460b0a80308cc5205002c
caratRED <ganesh@caratred.com>
2021-04-19 17:23:08 +0530

    sample

--- version2_app/version2_app/doctype/getUserRoles.py
+++ version2_app/version2_app/doctype/getUserRoles.py
@@ -1,7 +1,14 @@
 import frappe
 import json
 import requests
-
+from datetime import date
+import requests
+import datetime
+import random
+import traceback
+from frappe.utils import get_site_name
+import pandas as pd
+import numpy as np
 
 @frappe.whitelist(allow_guest=True)
 def getUserRoles():
@@ -43,3 +50,26 @@ def getUserRoles():
 			frappe.db.commit()
 		return {"success":True}
 		
+@frappe.whitelist(allow_guest=True)
+def samplebulk():
+	folder_path = frappe.utils.get_bench_path()
+	items_data_file = "/private/files/EINVOICE 01.03.2021 to 01.03.2021.xlsx"
+	company = "TLND-01"
+	companyData = frappe.get_doc('company',company)
+	site_folder_path = companyData.site_name
+	items_file_path = folder_path+'/sites/'+site_folder_path+items_data_file
+	items_dataframe = pd.read_excel(items_file_path)
+	# print(items_dataframe[''])
+	# list(dataPd['2'])
+	columnslist = items_dataframe.columns.values.tolist()
+	columnslist = columnslist[0].split("|")
+	print(columnslist,len(columnslist))
+	listdata = items_dataframe.head(3)
+	# print(listdata)
+	valuesdata = items_dataframe.values.tolist()
+	print(valuesdata[0],len(valuesdata),type(valuesdata))
+	print("--------")
+	val = valuesdata[0][0].split("|")
+	print(val,len(val))
+	return True		
+	# apps/version2_app/version2_app/version2_app/doctype/getUserRoles.py
