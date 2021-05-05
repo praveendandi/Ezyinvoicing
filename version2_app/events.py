f7fae71bfee491fd2b37c7e538abec9145ad0723
caratRED <ganesh@caratred.com>
2021-05-05 11:47:35 +0530

    gsp metering

--- version2_app/events.py
+++ version2_app/events.py
@@ -14,177 +14,177 @@ from datetime import date, timedelta
 
 
 def invoice_created(doc, method=None):
-    print("Invoice Created",doc.name)
-    if frappe.db.exists('Invoice Reconciliations', doc.name):
-        reconciliations_doc = frappe.get_doc('Invoice Reconciliations', doc.name)
-        reconciliations_doc.invoice_found = "Yes"
-        reconciliations_doc.save(ignore_permissions=True,ignore_version=True)
-    if doc.invoice_from=="Pms":
-        bin_name = frappe.db.get_value('Document Bin',{'invoice_file': doc.invoice_file})
-        bin_doc = frappe.get_doc("Document Bin",bin_name)
-        bin_doc.print_by = doc.print_by
-        bin_doc.document_type = doc.invoice_category
-        bin_doc.invoice_number = doc.name
-        # bin_doc.error_log = error_log
-        bin_doc.document_printed = "Yes"
-        bin_doc.save(ignore_permissions=True,ignore_version=True)
+	print("Invoice Created",doc.name)
+	if frappe.db.exists('Invoice Reconciliations', doc.name):
+		reconciliations_doc = frappe.get_doc('Invoice Reconciliations', doc.name)
+		reconciliations_doc.invoice_found = "Yes"
+		reconciliations_doc.save(ignore_permissions=True,ignore_version=True)
+	if doc.invoice_from=="Pms":
+		bin_name = frappe.db.get_value('Document Bin',{'invoice_file': doc.invoice_file})
+		bin_doc = frappe.get_doc("Document Bin",bin_name)
+		bin_doc.print_by = doc.print_by
+		bin_doc.document_type = doc.invoice_category
+		bin_doc.invoice_number = doc.name
+		# bin_doc.error_log = error_log
+		bin_doc.document_printed = "Yes"
+		bin_doc.save(ignore_permissions=True,ignore_version=True)
 
 def company_created(doc,method=None):
-    doc = frappe.db.get_list('company',filters={"docstatus":0},fields=["name","company_name","company_code","phone_number","gst_number","provider","ip_address","port"])
-    api="http://"+doc[0]["ip_address"]+":"+doc[0]["port"]+"/api/resource/Properties"
-    adequare_doc=frappe.get_doc("GSP APIS",doc[0]["provider"])
-    insert_dict={"doctype":"Properties","property_name":doc[0]["company_name"],"property_code":doc[0]["company_code"],"contact_number":doc[0]["phone_number"],"gst_number":doc[0]["gst_number"],"gsp_provider":doc[0]["provider"],"api_key":adequare_doc.gsp_prod_app_secret,"api_secret":adequare_doc.gsp_prod_app_id,"gsp_test_app_id":adequare_doc.gsp_test_app_id,"gsp_test_app_secret":adequare_doc.gsp_test_app_secret}
-    headers = {'content-type': 'application/json'}
-    r = requests.post(api,headers=headers,json=insert_dict)
+	doc = frappe.db.get_list('company',filters={"docstatus":0},fields=["name","company_name","company_code","phone_number","gst_number","provider","ip_address","port"])
+	api="http://"+doc[0]["ip_address"]+":"+doc[0]["port"]+"/api/resource/Properties"
+	adequare_doc=frappe.get_doc("GSP APIS",doc[0]["provider"])
+	insert_dict={"doctype":"Properties","property_name":doc[0]["company_name"],"property_code":doc[0]["company_code"],"contact_number":doc[0]["phone_number"],"gst_number":doc[0]["gst_number"],"gsp_provider":doc[0]["provider"],"api_key":adequare_doc.gsp_prod_app_secret,"api_secret":adequare_doc.gsp_prod_app_id,"gsp_test_app_id":adequare_doc.gsp_test_app_id,"gsp_test_app_secret":adequare_doc.gsp_test_app_secret}
+	headers = {'content-type': 'application/json'}
+	r = requests.post(api,headers=headers,json=insert_dict)
 
 def invoice_deleted(doc,method=None):
-    frappe.publish_realtime("custom_socket", {'message':'Invoice deleted','type':"Delete invoice","invoice_number":doc.name,"company":doc.company})
-    soc_doc = frappe.new_doc("Socket Notification")
-    soc_doc.invoice_number = doc.name
-    soc_doc.guest_name = doc.guest_name
-    soc_doc.document_type = doc.invoice_category
-    soc_doc.room_number = doc.room_number
-    soc_doc.confirmation_number = doc.confirmation_number
-    soc_doc.print_by = doc.print_by
-    soc_doc.invoice_category = doc.invoice_category
-    soc_doc.record_type = "Delete"
-    soc_doc.insert(ignore_permissions=True)
+	frappe.publish_realtime("custom_socket", {'message':'Invoice deleted','type':"Delete invoice","invoice_number":doc.name,"company":doc.company})
+	soc_doc = frappe.new_doc("Socket Notification")
+	soc_doc.invoice_number = doc.name
+	soc_doc.guest_name = doc.guest_name
+	soc_doc.document_type = doc.invoice_category
+	soc_doc.room_number = doc.room_number
+	soc_doc.confirmation_number = doc.confirmation_number
+	soc_doc.print_by = doc.print_by
+	soc_doc.invoice_category = doc.invoice_category
+	soc_doc.record_type = "Delete"
+	soc_doc.insert(ignore_permissions=True)
 
 def invoiceCreated(doc):
-    try:
-        # frappe.publish_realtime("invoice_created", "message")
-        print("=================---------------000000000000")       
-        frappe.publish_realtime("custom_socket", {'message':'Invoices Created','data':doc.__dict__,"company":doc.company})
-        soc_doc = frappe.new_doc("Socket Notification")
-        soc_doc.invoice_number = doc.name
-        soc_doc.guest_name = doc.guest_name
-        soc_doc.document_type = doc.invoice_category
-        soc_doc.room_number = doc.room_number
-        soc_doc.confirmation_number = doc.confirmation_number
-        soc_doc.print_by = doc.print_by
-        soc_doc.invoice_category = doc.invoice_category
-        soc_doc.record_type = "Create"
-        soc_doc.insert(ignore_permissions=True)
-        
-        filename = doc.invoice_file
-        bin_name = frappe.db.get_value('Document Bin',{'invoice_file': filename})
-               
-        bin_doc = frappe.get_doc("Document Bin",bin_name)
-        bin_doc.print_by = doc.print_by
-        bin_doc.document_printed = "Yes"
-        bin_doc.document_type = doc.invoice_category
-        bin_doc.invoice_number = doc.invoice_number
-        bin_doc.save(ignore_permissions=True,ignore_version=True)
-    except Exception as e:
-        print(str(e), "Invoice Created Socket Method")
-        print(traceback.print_exc())
-        return {"success":False,"message":str(e)}
+	try:
+		# frappe.publish_realtime("invoice_created", "message")
+		print("=================---------------000000000000")       
+		frappe.publish_realtime("custom_socket", {'message':'Invoices Created','data':doc.__dict__,"company":doc.company})
+		soc_doc = frappe.new_doc("Socket Notification")
+		soc_doc.invoice_number = doc.name
+		soc_doc.guest_name = doc.guest_name
+		soc_doc.document_type = doc.invoice_category
+		soc_doc.room_number = doc.room_number
+		soc_doc.confirmation_number = doc.confirmation_number
+		soc_doc.print_by = doc.print_by
+		soc_doc.invoice_category = doc.invoice_category
+		soc_doc.record_type = "Create"
+		soc_doc.insert(ignore_permissions=True)
+		
+		filename = doc.invoice_file
+		bin_name = frappe.db.get_value('Document Bin',{'invoice_file': filename})
+			   
+		bin_doc = frappe.get_doc("Document Bin",bin_name)
+		bin_doc.print_by = doc.print_by
+		bin_doc.document_printed = "Yes"
+		bin_doc.document_type = doc.invoice_category
+		bin_doc.invoice_number = doc.invoice_number
+		bin_doc.save(ignore_permissions=True,ignore_version=True)
+	except Exception as e:
+		print(str(e), "Invoice Created Socket Method")
+		print(traceback.print_exc())
+		return {"success":False,"message":str(e)}
 
 
 
 def update_documentbin(filepath, error_log):
-    try:
-        bin_data = frappe.db.get_list('Document Bin', filters={'invoice_file': ['=', filepath]})
-        print(bin_data)
-        if len(bin_data)>0:
-            pass
-        else:
-            if '@' in filepath:
-                systemName = re.search('@(.*)@', filepath)
-                systemName = systemName.group(1)
-            else:
-                systemName = "NA"    
-            bin_doc = frappe.new_doc("Document Bin")
-            bin_doc.system_name = systemName
-            bin_doc.invoice_file = filepath
-            bin_doc.error_log =  error_log
-            bin_doc.insert(ignore_permissions=True)
-            frappe.db.commit()
-    except Exception as e:
-        print(str(e), "update_documentbin")
-        print(traceback.print_exc())
-        return {"success":False,"message":str(e)}
+	try:
+		bin_data = frappe.db.get_list('Document Bin', filters={'invoice_file': ['=', filepath]})
+		print(bin_data)
+		if len(bin_data)>0:
+			pass
+		else:
+			if '@' in filepath:
+				systemName = re.search('@(.*)@', filepath)
+				systemName = systemName.group(1)
+			else:
+				systemName = "NA"    
+			bin_doc = frappe.new_doc("Document Bin")
+			bin_doc.system_name = systemName
+			bin_doc.invoice_file = filepath
+			bin_doc.error_log =  error_log
+			bin_doc.insert(ignore_permissions=True)
+			frappe.db.commit()
+	except Exception as e:
+		print(str(e), "update_documentbin")
+		print(traceback.print_exc())
+		return {"success":False,"message":str(e)}
 
 
 def fileCreated(doc, method=None):
-    try:
-        if 'job-' in doc.file_name:
-            if not frappe.db.exists({'doctype': 'Document Bin','invoice_file': doc.file_url}):
-                update_documentbin(doc.file_url,"")
-                abs_path = os.path.dirname(os.getcwd())
-                company_doc = frappe.get_doc("company",doc.attached_to_name)
-                new_parsers = company_doc.new_parsers
-                if new_parsers == 0:
-                    file_path = abs_path + '/apps/version2_app/version2_app/parsers/'+doc.attached_to_name+'/invoice_parser.py'
-                else:
-                    file_path = abs_path + '/apps/version2_app/version2_app/parsers_invoice/invoice_parsers/'+doc.attached_to_name+'/invoice_parser.py'
-                module_name = 'file_parsing'
-                spec = importlib.util.spec_from_file_location(module_name, file_path)
-                module = importlib.util.module_from_spec(spec)
-                spec.loader.exec_module(module)
-                module.file_parsing(doc.file_url)
-        else:
-            if ".pdf" in doc.file_url and "with-qr" not in doc.file_url:
-                update_documentbin(doc.file_url,"")
+	try:
+		if 'job-' in doc.file_name:
+			if not frappe.db.exists({'doctype': 'Document Bin','invoice_file': doc.file_url}):
+				update_documentbin(doc.file_url,"")
+				abs_path = os.path.dirname(os.getcwd())
+				company_doc = frappe.get_doc("company",doc.attached_to_name)
+				new_parsers = company_doc.new_parsers
+				if new_parsers == 0:
+					file_path = abs_path + '/apps/version2_app/version2_app/parsers/'+doc.attached_to_name+'/invoice_parser.py'
+				else:
+					file_path = abs_path + '/apps/version2_app/version2_app/parsers_invoice/invoice_parsers/'+doc.attached_to_name+'/invoice_parser.py'
+				module_name = 'file_parsing'
+				spec = importlib.util.spec_from_file_location(module_name, file_path)
+				module = importlib.util.module_from_spec(spec)
+				spec.loader.exec_module(module)
+				module.file_parsing(doc.file_url)
+		else:
+			if ".pdf" in doc.file_url and "with-qr" not in doc.file_url:
+				update_documentbin(doc.file_url,"")
 
-            print('Normal File')
-    except Exception as e:
-        print(str(e), "fileCreated")
-        update_documentbin(doc.file_url,str(e))
-        print(traceback.print_exc())
-        return {"success":False,"message":str(e)}
+			print('Normal File')
+	except Exception as e:
+		print(str(e), "fileCreated")
+		update_documentbin(doc.file_url,str(e))
+		print(traceback.print_exc())
+		return {"success":False,"message":str(e)}
 
 def Updateemitsocket(doc,method=None):
-    if doc.status=="Success":
-        company = frappe.get_last_doc('company')
-        frappe.log_error("trigger socket bench update", " {'message':'bench  update started','type':'bench update'}")
-        frappe.publish_realtime("custom_socket", {'message':'bench  update started','type':"bench update","company":company.name})
+	if doc.status=="Success":
+		company = frappe.get_last_doc('company')
+		frappe.log_error("trigger socket bench update", " {'message':'bench  update started','type':'bench update'}")
+		frappe.publish_realtime("custom_socket", {'message':'bench  update started','type':"bench update","company":company.name})
 
 
 def DocumentBinSocket(doc,method=None):
-    company = frappe.get_last_doc('company')
-    frappe.log_error("Document Bin Insert", " {'message':'Docuemnt Bin Insert'}")
-    frappe.publish_realtime("custom_socket", {'message':'Document Bin Insert','type':"document_bin_insert","data":doc.__dict__,"company":company.name})
+	company = frappe.get_last_doc('company')
+	frappe.log_error("Document Bin Insert", " {'message':'Docuemnt Bin Insert'}")
+	frappe.publish_realtime("custom_socket", {'message':'Document Bin Insert','type':"document_bin_insert","data":doc.__dict__,"company":company.name})
 
 def updateManager(doc, method=None):
-    try:
-        if doc.status!="Ongoing":
-            print("==========")
-            commands = ['git pull origin updates','systemctl nginx reload','systemctl nginx restart']
-            console_dump = ''
-            company = frappe.get_last_doc('company')
-            print(company)
-            # cwd = '/home/caratred/Documents/angular/ezy-invoice-production'
-            # cwd = '/home/frappe/ezy-invoice-production'
-            cwd = company.angular_project_production_path
-            key = str(time.time())
-            # count = 0
-            for command in commands:
-                print(command,"    command")
-                terminal = Popen(shlex.split(command),
-                                stdin=PIPE,
-                                stdout=PIPE,
-                                stderr=STDOUT,
-                                cwd=cwd)
-                print(terminal,"//////////")                
-                # frappe.log_error("log error", terminal.stdout.read(1))
-                for c in iter(lambda: safe_decode(terminal.stdout.read(1)), ''):
-                    console_dump += c
-            logged_command = " && ".join(commands)
-            frappe.publish_realtime("custom_socket", {'message':'bench update completed','type':"bench completed","company":company.name})
-            # frappe.log_error("Angular project pull", console_dump)
-            frappe.log_error("Angular project pull data","update manager")
-    except Exception as e:
-        print(str(e),"    updateManager")
+	try:
+		if doc.status!="Ongoing":
+			print("==========")
+			commands = ['git pull origin updates','systemctl nginx reload','systemctl nginx restart']
+			console_dump = ''
+			company = frappe.get_last_doc('company')
+			print(company)
+			# cwd = '/home/caratred/Documents/angular/ezy-invoice-production'
+			# cwd = '/home/frappe/ezy-invoice-production'
+			cwd = company.angular_project_production_path
+			key = str(time.time())
+			# count = 0
+			for command in commands:
+				print(command,"    command")
+				terminal = Popen(shlex.split(command),
+								stdin=PIPE,
+								stdout=PIPE,
+								stderr=STDOUT,
+								cwd=cwd)
+				print(terminal,"//////////")                
+				# frappe.log_error("log error", terminal.stdout.read(1))
+				for c in iter(lambda: safe_decode(terminal.stdout.read(1)), ''):
+					console_dump += c
+			logged_command = " && ".join(commands)
+			frappe.publish_realtime("custom_socket", {'message':'bench update completed','type':"bench completed","company":company.name})
+			# frappe.log_error("Angular project pull", console_dump)
+			frappe.log_error("Angular project pull data","update manager")
+	except Exception as e:
+		print(str(e),"    updateManager")
 
-        
+		
 
 def safe_decode(string, encoding='utf-8'):
-    try:
-        string = string.decode(encoding)
-    except Exception:
-        pass
-    return string
+	try:
+		string = string.decode(encoding)
+	except Exception:
+		pass
+	return string
 
 def deleteemailfilesdaily():
 	try:
@@ -199,18 +199,33 @@ def deleteemailfilesdaily():
 			filelist.append(value[0]['fid'])
 			delete = frappe.delete_doc("File",value[0]['fid'])
 			print(delete)
-
+		lastdate = date.today() - timedelta(days=6)
+		data = frappe.db.sql("""DELETE FROM `tabDocument Bin` WHERE creation < %s""",lastdate)
+		print(data)
+		frappe.db.commit()
 		return {"success":True}
 	except Exception as e:
 		return {"success":False,"message":str(e)}
 
 
 def gspmeteringhook(doc,method=None):
-    try:
-        company = frappe.get_doc('company',doc.company)
-        inputData = {"data":{"doctype":"Gsp Metering","property_code":doc.company,'tax_payer_details':doc.tax_payer_details,'login':doc.login,'generate_irn':doc.generate_irn,'get_irn_details_by_doc':doc.get_irn_details_by_doc,'cancel_irn':doc.cancel_irn,'invoice_by_irn':doc.invoice_by_irn,'create_qr_image':doc.create_qr_image,'status':doc.status}}
-        headers = {'Content-Type': 'application/json'}
-        json_response = requests.post(company.licensing_host+"/api/method/ezylicensing.ezylicensing.getcount.gspmetering_post",headers=headers,json=inputData)
-        return json_response            
-    except Exception as e:
-        print(str(e))    
+	try:
+		company = frappe.get_doc('company',doc.company)
+		inputData = {"data":{"doctype":"Gsp Metering","property_code":doc.company,'tax_payer_details':doc.tax_payer_details,'login':doc.login,'generate_irn':doc.generate_irn,'get_irn_details_by_doc':doc.get_irn_details_by_doc,'cancel_irn':doc.cancel_irn,'invoice_by_irn':doc.invoice_by_irn,'create_qr_image':doc.create_qr_image,'status':doc.status}}
+		headers = {'Content-Type': 'application/json'}
+		json_response = requests.post(company.licensing_host+"/api/method/ezylicensing.ezylicensing.getcount.gspmetering_post",headers=headers,json=inputData)
+		return json_response            
+	except Exception as e:
+		print(str(e))    
+
+
+def dailyDeletedocumentBin():
+	try:
+		company = frappe.get_last_doc('company')
+		lastdate = date.today() - timedelta(days=6)
+		data = frappe.db.sql("""DELETE FROM `tabDocument Bin` WHERE creation < %s""",lastdate)
+		print(data)
+		frappe.db.commit()
+		return {"success":True}
+	except Exception as e:
+		return {"success":False,"message":str(e)}	
--- version2_app/version2_app/doctype/gsp_metering/gsp_metering.py
+++ version2_app/version2_app/doctype/gsp_metering/gsp_metering.py
@@ -3,8 +3,107 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
 from frappe.model.document import Document
+import calendar
+import frappe
+import traceback
+import datetime
+from dateutil.relativedelta import relativedelta
+
 
 class GspMetering(Document):
 	pass
+
+
+@frappe.whitelist(allow_guest=True)
+def gspmeteringdata(data):
+	try:
+		date=datetime.datetime.today()
+		outputdata=[]
+		if data["range"]=="this week":
+			start_day = (date - datetime.timedelta(days=date.weekday())).date().day
+			end_day=date.day
+		elif data["range"]== "last week":
+			from_date = (date + datetime.timedelta(-date.weekday(), weeks=-1)).date()
+			to_date = (date + datetime.timedelta(-date.weekday() - 1)).date()
+			start_day = from_date.day
+			end_day=to_date.day
+		elif data["range"]=="this month":
+			start_day=1
+			end_day=date.day
+		elif data["range"]=="last month":
+			date=datetime.datetime.now()
+			d = date - relativedelta(months=1)
+			from_date = datetime.date(d.year, d.month, 1)
+			today=date.today()
+			to_date = datetime.date(today.year, today.month, 1) - relativedelta(days=1)
+			start_day=from_date.day
+			end_day=to_date.day
+		elif data["range"]=="this year":
+			start_day=1
+			end_day=date.month
+		fields_list=["tax_payer_details","cancel_irn","get_irn_details_by_doc","login","invoice_by_irn","sync_gst_details","generate_irn"]
+		data_dict={"day_wise":[]}
+		day_wise={}
+		print(start_day,end_day)
+		for each in range(start_day,end_day+1):
+			if each<10:
+				each = '0'+str(each)
+			else:
+				each = str(each)
+			if data["range"]=="last month":
+				start_date=str(date.year)+'-'+str(date.month-1)+'-'+str(start_day)
+				end_date=str(date.year)+'-'+str(date.month-1)+'-'+str(end_day)
+				indate = str(date.year)+'-'+str(date.month-1)+'-'+each
+			elif data["range"]=="this year":
+				start_date=1
+				end_date=date.month
+				year=date.year
+				date=datetime.datetime.strptime(str(each),"%m")
+				indate=date.strftime("%B")
+			else:
+				start_date=str(date.year)+'-'+str(date.month)+'-'+str(start_day)
+				end_date=str(date.year)+'-'+str(date.month)+'-'+str(end_day)
+				indate =str(date.year)+'-'+str(date.month)+'-'+each
+			for fields in fields_list:
+				if data["range"]=="this year":
+					get_data=frappe.db.sql("""select count(name) as total_{} from `tabGsp Metering` where {}="True" and year(creation)='{}' group by year(creation) """.format(fields,fields,year),as_dict=1)
+					if get_data!=[]:
+						data_dict["total_{}".format(fields)]=get_data[0]["total_{}".format(fields)]
+					else:
+						pass
+					year=datetime.datetime.today().strftime("%Y")
+					data1=frappe.db.sql("""select count(name) as {},monthname(creation) as creation from `tabGsp Metering` where month(creation)={} and year(creation)={} and {}="True" group by '{}' """.format(fields,each,year,fields,fields),as_dict=1)
+					
+				else:
+					get_data=frappe.db.get_list("Gsp Metering",filters={"creation":["between",[start_date,end_date]],fields:"True"},fields=["count(name) as total_{}".format(fields)])
+					data_dict["total_{}".format(fields)]=get_data[0]["total_{}".format(fields)]
+					data1=frappe.db.get_list("Gsp Metering",filters={"creation":["between",[indate,indate]],fields:"True"},fields=["count(name) as {}".format(fields),"creation"],group_by=fields)
+				if data1 != []:
+					if indate in day_wise.keys():
+						day_wise[indate].extend(data1)
+					else:
+						day_wise[indate]=data1
+		for key,val in day_wise.items():
+			if val != []:
+				result={}
+				result.update({"creation":key})
+				for i in val:
+					result.update(i)
+				data_dict["day_wise"].append(result)
+			else:
+				data_dict["day_wise"].append({})
+		if len(data_dict["day_wise"])!=end_day-start_day:
+			for i in range(len(data_dict["day_wise"]),(end_day-start_day)+1):
+				data_dict["day_wise"].append({})
+		return {"success":True,"data":data_dict}
+	except Exception as e:
+		print(traceback.print_exc())
+		return {"status":False,"message":str(e)}  
+
+
+# @frappe.whitelist(allow_guest=True)
+# def gspmetering_post(data):
+# 	propertyData = frappe.get_doc(Properties,data['property_code'])
+# 	get_gsp=frappe.get_doc(data)
+# 	get_gsp.insert()
