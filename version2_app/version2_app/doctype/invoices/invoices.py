753ebfbb3437765dfa90cd0820e8ca165045c519
prasanthvajja <prasanth@caratred.com>
2021-06-11 13:21:48 +0530

    code updated

--- version2_app/version2_app/doctype/invoices/invoices.py
+++ version2_app/version2_app/doctype/invoices/invoices.py
@@ -2710,6 +2710,7 @@ def get_tax_payer_details(data):
     get TaxPayerDetail from gsp   gstNumber, code, apidata
     '''
     try:
+        print(data,"========================")
         company = frappe.get_doc('company',data['code'])
         headers = {'Content-Type': 'application/json'}
         tay_payer_details = frappe.db.get_value('TaxPayerDetail',data['gstNumber'])
