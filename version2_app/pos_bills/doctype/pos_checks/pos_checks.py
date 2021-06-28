6cd0df9396fbc75e6076a1bb66f70528f7917993
prasanthvajja <prasanth@caratred.com>
2021-06-28 17:29:18 +0530

    modified changes

--- version2_app/pos_bills/doctype/pos_checks/pos_checks.py
+++ version2_app/pos_bills/doctype/pos_checks/pos_checks.py
@@ -7,7 +7,7 @@ import frappe
 from frappe.model.document import Document
 from frappe.utils import logger
 import traceback, sys
-import os
+import os,re
 from escpos.printer import Network
 frappe.utils.logger.set_log_level("DEBUG")
 logger = frappe.logger("api", allow_site=True, file_count=50)
@@ -19,19 +19,21 @@ class POSChecks(Document):
 @frappe.whitelist(allow_guest=True)
 def create_pos_bills(data):
 	try:
-		print(data,"---------------------------")
 		company_doc = frappe.get_doc("company",data["company"])
 		data["mode"] = company_doc.mode
 		data["doctype"] = "POS Checks"
 		outlet_doc = frappe.get_doc("Outlets",data["outlet"])
-		outlet_printer = frappe.db.get_value("POS Print Settings",{"outlet":outlet_doc.name},"printer")	
+		outlet_printer = frappe.db.get_value("POS Print Settings",{"outlet":outlet_doc.name},"printer") 
 		if outlet_doc.print == "Yes":
 			folder_path = frappe.utils.get_bench_path()
 			path = folder_path + '/sites/' + company_doc.site_name +"/public"
 			logopath = path+outlet_doc.outlet_logo
 			qrpath = path+outlet_doc.static_payment_qr_code
-			print(logopath, qrpath)
 			give_print(data["payload"],outlet_printer,logopath,qrpath)
+		# extract_bills = extract_data(data["payload"],outlet_doc)
+		# if extract_bills["success"] == False:
+		# 	return extract_bills["message"]
+		# data.update(extract_bills["data"])
 		doc = frappe.get_doc(data)
 		doc.insert(ignore_permissions=True,ignore_links=True)
 	except Exception as e:
@@ -39,6 +41,45 @@ def create_pos_bills(data):
 		frappe.log_error("Ezy-invoicing create pos bills","line No:{}\n{}".format(exc_tb.tb_lineno,traceback.format_exc()))
 		return {"success":False,"message":str(e)}
 
+
+# def extract_data(payload,outlet_doc):
+# 	try:
+# 		data={}
+# 		raw_data = payload.split("\n")
+# 		total_amount = ""
+# 		for line in raw_data:
+# 			if outlet_doc.closed in payload and outlet_doc.void not in payload:
+# 				data["check_type"] = "Check Closed"
+# 				total_amount = "0.00"
+# 			elif outlet_doc.void in payload:
+# 				data["check_type"] = "Closed Void Check"
+# 				if re.match("[ChangeDue\s]+[A-Za-z.0-9]+",line.strip()):
+# 					total_amount_regex = re.findall("\d.+",line.replace(" ",""))
+# 					total_amount = total_amount_regex[0] if len(total_amount_regex) > 0 else ""
+# 			else:
+# 				data["check_type"] = "Normal Check"
+# 				if re.match("[TotalDue\s]+[A-Za-z.0-9]+",line.strip()):
+# 					total_amount_regex = re.findall("\d.+",line.replace(" ",""))
+# 					total_amount = total_amount_regex[0] if len(total_amount_regex) > 0 else ""
+# 			if "CHK" in line and "TBL" in line:
+# 				check_no = re.search('CHK(.*)TBL', line.strip())
+# 				data["check_no"] = (check_no.group(1)).strip()
+# 			if "TBL" in line and "GST" in line:
+# 				table_no = re.search('TBL(.*)GST', line.strip())
+# 				data["table_number"] = (table_no.group(1)).strip()
+# 			if "GST" in line and "GST No." not in line and "%" not in line:
+# 				guest_count = line.split(" ")[-1]
+# 				data["no_of_guests"] = guest_count
+# 			if "TBL" in line and "GST" not in line:
+# 				table_no = line.split(" ")[-1]
+# 				data["table_number"] = table_no
+# 		data["total_amount"] = str(total_amount)
+# 		return {"success":True, "data":data}
+# 	except Exception as e:
+# 		print(str(e))
+# 		exc_type, exc_obj, exc_tb = sys.exc_info()
+# 		frappe.log_error("Ezy-invoicing extract data","line No:{}\n{}".format(exc_tb.tb_lineno,traceback.format_exc()))
+# 		return {"success":False,"message":str(e)}
 
 def give_print(text, ip, logo_path, qr_path):
 	try:
