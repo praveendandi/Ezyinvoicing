da7273ca3637f1577cdbfced10553d8866d6e9ee
kiran <kiran@caratred.com>
2021-08-11 10:49:27 +0530

    updated scheduler for 11

--- version2_app/hooks.py
+++ version2_app/hooks.py
@@ -141,7 +141,7 @@ scheduler_events = {
 	"corn":{"0 1 * * *":["version2_app.events.dailyDeletedocumentBin"],
 			"10 1 * * * ":["version2_app.events.deleteemailfilesdaily"],
 			"20 1 * * *":["version2_app.events.dailyIppprinterFiles"],
-			"45 10 * * *":["version2_app.events.block_irn"]}	
+			"0 11 * * *":["version2_app.events.block_irn"]}	
 }	
 
 
