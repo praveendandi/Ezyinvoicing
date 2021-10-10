d41fe4103d8971a67c878b5cc1dadbe4b09dbf04
kiran <kiran@caratred.com>
2021-10-10 09:05:45 +0530

    added watcher file

---
+++ arrival_watcher.py
@@ -0,0 +1,62 @@
+import json
+import threading
+import requests
+import glob
+import os
+from os import path
+import time,json
+
+config_json_path="/home/caratred/Desktop/config/config.json"
+headers = {'Content-Type': 'application/json'}
+with open(config_json_path) as f:
+   config_json = json.load(f)
+def getprearrivals_file():
+    
+    try:
+        print("Waiting for File")
+        time.sleep(5)
+        files = glob.glob(config_json["file_path"])
+        print("File Read Succesfully")
+        for file_path in files:
+            invoicefile = {'file': open(file_path, 'rb')}
+            payload = {
+                'is_private': 1,
+                'folder': 'Home',
+                'doctype': 'invoices',
+                'docname': config_json["name"],
+                'fieldname': 'invoice'}
+            if config_json["proxy"] == 1:
+                print("Proxy is Active")
+                proxyhost = config_json["proxy_url"]
+                proxyhost = proxyhost.replace("http://","@")
+                proxies = {'http':'http://'+config_json["proxy_username"]+":"+config_json["proxy_password"]+proxyhost,
+                                'https':'https://'+config_json["proxy_username"]+":"+config_json["proxy_password"]+proxyhost}
+                file_response = requests.get(config_json["host"]+"api/method/upload_file",headers=headers,proxies=proxies,verify=False)
+                print("File Uploaded to Host")
+            else:
+                if config_json["skip_ssl_verify"] == 1:
+                    file_response = requests.post(config_json["host"]+"api/method/upload_file",files=invoicefile, data=payload, verify=False)
+                else:
+                    file_response = requests.post(config_json["host"]+"api/method/upload_file",files=invoicefile, data=payload, verify=False)
+            if file_response.status_code==200:
+                file_data = file_response.json()
+                print(file_response,"File response")
+                if config_json["proxy"] == 1:
+                    proxyhost = config_json["proxy_url"]
+                    proxyhost = proxyhost.replace("http://","@")
+                    proxies = {'http':'http://'+config_json["proxy_username"]+":"+config_json["proxy_password"]+proxyhost,
+                                    'https':'https://'+config_json["proxy_username"]+":"+config_json["proxy_password"]+proxyhost}
+                    file_response_arr = requests.get(config_json["host"]+"api/method/version2_app.arrivals.doctype.arrival_information.arrival_information.arrivalActivity",params={"company":file_data["message"]["attached_to_name"],"file_url":file_data["message"]["file_url"],"source":"Manual"})
+                else:
+                    if config_json["skip_ssl_verify"] == 1:
+                        file_response_arr = requests.post(config_json["host"]+"api/method/version2_app.arrivals.doctype.arrival_information.arrival_information.arrivalActivity",params={"company":file_data["message"]["attached_to_name"],"file_url":file_data["message"]["file_url"],"source":"Manual"})
+                    else:
+                        file_response_arr = requests.post(config_json["host"]+"api/method/version2_app.arrivals.doctype.arrival_information.arrival_information.arrivalActivity",params={"company":file_data["message"]["attached_to_name"],"file_url":file_data["message"]["file_url"],"source":"Manual"})
+            invoicefile['file'].close()
+            print("file removed")
+            if path.exists(file_path):
+                os.remove(file_path)
+    except Exception as e:
+        print(e,"am from exception")
+    getprearrivals_file()
+getprearrivals_file()
--- watcher.py
+++ watcher.py
@@ -1,57 +1,60 @@
-import json
 import threading
 import requests
 import glob
 import os
-import frappe
 from os import path
-import time,json
-from version2_app.arrivals.doctype.arrival_information.arrival_information import arrivalActivity
+import time
+# def printit():
+# 	threading.Timer(5.0, printit).start()
+# 	files = glob.glob("/home/caratred/test/*.pdf")
+# 	print(files)
+# 	for file_path in files:
+# 		data = {"company":"EHNDNP-01",
+# 							"host":"http://0.0.0.0:8000/api/method/",}
+# 		invoicefile = {'file': open(file_path, 'rb')}
+# 		payload = {
+# 				'is_private': 1,
+# 				'folder': 'Home',
+# 				'doctype': 'invoices',
+# 				'docname': data["company"],
+# 				'fieldname': 'invoice'}
+# 		file_response = requests.post(data['host']+"upload_file",files=invoicefile, data=payload, verify=False).json()
+# 		print(file_response)
+# 		if file_response:
+# 			if path.exists(file_path):
+# 				os.remove(file_path)
+# printit()
 
-config_json_path="/home/caratred/Desktop/config/config.json"
-headers = {'Content-Type': 'application/json'}
-with open(config_json_path) as f:
-   config_json = json.load(f)
-def getprearrivals_file():
+def getfiles():
     try:
         time.sleep(5)
-        files = glob.glob(config_json["file_path"])
+        print("waiting for print")
+        files = glob.glob("/home/frappe/files/*.pdf")
         for file_path in files:
+            data = {"company":"MJH-01","host":"http://0.0.0.0:8000/api/method/"}
+            print(data,"config data")
             invoicefile = {'file': open(file_path, 'rb')}
             payload = {
                 'is_private': 1,
                 'folder': 'Home',
                 'doctype': 'invoices',
-                'docname': config_json["name"],
+                'docname': data["company"],
                 'fieldname': 'invoice'}
-            if config_json["proxy"] == 1:
-                proxyhost = config_json["proxy_url"]
-                proxyhost = proxyhost.replace("http://","@")
-                proxies = {'http':'http://'+config_json["proxy_username"]+":"+config_json["proxy_password"]+proxyhost,
-                                'https':'https://'+config_json["proxy_username"]+":"+config_json["proxy_password"]+proxyhost}
-                file_response = requests.get(config_json["host"]+"api/method/upload_file",headers=headers,proxies=proxies,verify=False)
-            else:
-                if config_json["skip_ssl_verify"] == 1:
-                    file_response = requests.post(config_json["host"]+"api/method/upload_file",files=invoicefile, data=payload, verify=False)
-                else:
-                    file_response = requests.post(config_json["host"]+"api/method/upload_file",files=invoicefile, data=payload, verify=False)
-            if file_response.status_code==200:
-                file_data = file_response.json()
-                if config_json["proxy"] == 1:
-                    proxyhost = config_json["proxy_url"]
-                    proxyhost = proxyhost.replace("http://","@")
-                    proxies = {'http':'http://'+config_json["proxy_username"]+":"+config_json["proxy_password"]+proxyhost,
-                                    'https':'https://'+config_json["proxy_username"]+":"+config_json["proxy_password"]+proxyhost}
-                    file_response_arr = requests.get(config_json["host"]+"api/method/version2_app.arrivals.doctype.arrival_information.arrival_information.arrivalActivity",params={"company":file_data["message"]["attached_to_name"],"file_url":file_data["message"]["file_url"],"source":"Manual"})
-                else:
-                    if config_json["skip_ssl_verify"] == 1:
-                        file_response_arr = requests.post(config_json["host"]+"api/method/version2_app.arrivals.doctype.arrival_information.arrival_information.arrivalActivity",params={"company":file_data["message"]["attached_to_name"],"file_url":file_data["message"]["file_url"],"source":"Manual"})
-                    else:
-                        file_response_arr = requests.post(config_json["host"]+"api/method/version2_app.arrivals.doctype.arrival_information.arrival_information.arrivalActivity",params={"company":file_data["message"]["attached_to_name"],"file_url":file_data["message"]["file_url"],"source":"Manual"})
+            #invoicefile['file'].close()
+            file_response = requests.post(data['host']+"upload_file",files=invoicefile, data=payload, verify=False)
             invoicefile['file'].close()
+            print(file_response)
+            invoicefile['file'].close()
+            #if file_response:
             if path.exists(file_path):
+            #time.sleep(5)
                 os.remove(file_path)
     except Exception as e:
         print(e,"am from exception")
-    getprearrivals_file()
-getprearrivals_file()
+        #invoicefile['file'].close()
+        #if path.exists(file_path):
+            #os.remove(file_path)
+    getfiles()
+
+getfiles()
+
