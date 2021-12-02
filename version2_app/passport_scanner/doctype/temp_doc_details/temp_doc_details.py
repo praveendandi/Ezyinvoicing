b85de04091d66dc5a10619ef6a264a9f47089986
sumanth <sumanth@caratred.com>
2021-12-02 09:21:23 +0530

    added v2 detection

---
+++ version2_app/passport_scanner/doctype/temp_doc_details/temp_doc_details.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2021, caratred and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class TempDocDetails(Document):
+	pass
+
+
+
+
+def create_passport_details():
+	pass
