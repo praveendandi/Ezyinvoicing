fd5a5eeabff76d43295205e32488e250a5653ad7
prasanthvajja <prasanth@caratred.com>
2021-12-08 09:27:46 +0530

    modified company create event in events

--- version2_app/events.py
+++ version2_app/events.py
@@ -80,20 +80,20 @@ def company_created(doc,method=None):
     try:
         if frappe.db.exists('company',doc.name):
             pass
-            # doc = frappe.db.get_list('company',filters={"docstatus":0},fields=["name","company_name","company_code","phone_number","gst_number","provider","ip_address","port"])
-            # api="http://"+doc[0]["ip_address"]+":"+doc[0]["port"]+"/api/resource/Properties"
-            # adequare_doc=frappe.get_doc("GSP APIS",doc[0]["provider"])
-            # insert_dict={"doctype":"Properties","property_name":doc[0]["company_name"],"property_code":doc[0]["company_code"],"contact_number":doc[0]["phone_number"],"gst_number":doc[0]["gst_number"],"gsp_provider":doc[0]["provider"],"api_key":adequare_doc.gsp_prod_app_secret,"api_secret":adequare_doc.gsp_prod_app_id,"gsp_test_app_id":adequare_doc.gsp_test_app_id,"gsp_test_app_secret":adequare_doc.gsp_test_app_secret}
-            # headers = {'content-type': 'application/json'}
-            # r = requests.post(api,headers=headers,json=insert_dict,verify=False)
-            # folder_path = frappe.utils.get_bench_path()
-            # if doc.pms_property_logo != "":
-            #     file_path = folder_path+'/sites/'+doc.site_name+doc.pms_property_logo
-            #     status = upload_propery_logo_pms({"file_path":file_path,"company":doc.name})
-            #     if status["success"] == False:
-            #         return status 
-            #     frappe.db.set_value('company', doc.name, {"pms_property_url":status["data"]})
-            #     frappe.db.commit()
+            doc = frappe.db.get_list('company',filters={"docstatus":0},fields=["name","company_name","company_code","phone_number","gst_number","provider","licensing_host","port"])
+            api="http://"+doc[0]["licensing_host"]+":"+doc[0]["port"]+"/api/resource/Properties"
+            adequare_doc=frappe.get_doc("GSP APIS",doc[0]["provider"])
+            insert_dict={"doctype":"Properties","property_name":doc[0]["company_name"],"property_code":doc[0]["company_code"],"contact_number":doc[0]["phone_number"],"gst_number":doc[0]["gst_number"],"gsp_provider":doc[0]["provider"],"api_key":adequare_doc.gsp_prod_app_secret,"api_secret":adequare_doc.gsp_prod_app_id,"gsp_test_app_id":adequare_doc.gsp_test_app_id,"gsp_test_app_secret":adequare_doc.gsp_test_app_secret}
+            headers = {'content-type': 'application/json'}
+            r = requests.post(api,headers=headers,json=insert_dict,verify=False)
+            folder_path = frappe.utils.get_bench_path()
+            if doc.pms_property_logo != "":
+                file_path = folder_path+'/sites/'+doc.site_name+doc.pms_property_logo
+                status = upload_propery_logo_pms({"file_path":file_path,"company":doc.name})
+                if status["success"] == False:
+                    return status 
+                frappe.db.set_value('company', doc.name, {"pms_property_url":status["data"]})
+                frappe.db.commit()
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         frappe.log_error("Ezy-invoicing company_created Event","line No:{}\n{}".format(exc_tb.tb_lineno,traceback.format_exc()))   
