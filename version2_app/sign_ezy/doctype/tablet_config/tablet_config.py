a50cb849f853dad6d0c461682fa5e769b72f8e58
prasanthvajja <prasanth@caratred.com>
2021-12-13 18:04:12 +0530

    modified socket for disconnet tablet

--- version2_app/sign_ezy/doctype/tablet_config/tablet_config.py
+++ version2_app/sign_ezy/doctype/tablet_config/tablet_config.py
@@ -258,6 +258,8 @@ def disconnectWorkStation(ws):
             tablet_disconnected = disconnectTablet(get_tablet_config[0]["name"])
             if tablet_disconnected["success"] == False:
                 return tablet_disconnected
+            config_doc = frappe.get_doc("Tablet Config",get_tablet_config[0]["name"])
+            frappe.publish_realtime("custom_socket", {'message': 'Reset WorkStation', 'data': config_doc.__dict__})
             return {"success":True,"message":"Tablet mapped removed successfully"}
 
         else:
