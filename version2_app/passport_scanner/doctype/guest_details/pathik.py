04e626de472ee5ea0b086ca1ce3fbefa6af744c5
prasanthvajja <prasanth@caratred.com>
2022-01-21 18:27:18 +0530

    added pathik code

--- version2_app/passport_scanner/doctype/guest_details/pathik.py
+++ version2_app/passport_scanner/doctype/guest_details/pathik.py
@@ -18,6 +18,7 @@ add_guest_url = 'https://pathik.guru/dashboard/add'
 driver = {}
 
 global_delay = 30
+folder_path = frappe.utils.get_bench_path()
 
 guest_details = {
     "first_name": "sumanth",
@@ -59,113 +60,149 @@ total_guest_details = []
 
 def add_guest(driver):
     try:
-        data = guest_details
+        # data = guest_details
         driver.get(add_guest_url)
         for each in total_guest_details:
             if frappe.db.exists('Guest Details',each):
-                pass
-                # data = frappe.db.get_value("Guest Details",each,["given_name as first_name","middle_name","given_name as last_name",""], as_dict=1) 
-                # print(data)
-        first_name = driver.find_elements_by_name("first_name")
-        first_name[0].send_keys(data['first_name'])
+                data = frappe.db.get_value("Guest Details",each,["given_name as first_name","middle_name","given_name as last_name","guest_company_name as company_name","address","address2 as house_flat_no",
+                                                                 "pathik_locality as locality", "city","pathik_district as district","pathik_country as country","postal_code as zip_code","pathik_state as state","date_of_birth as dob","permanent_mobile_no as mobile_no",
+                                                                 "permanent_phone_no as phone_no","email","arrival_from_city as coming_from","next_destination_place as going_to","id_type as doc_type","passport_number","local_id_number","room_number as room_no",
+                                                                 "checkin_date","checkin_time","checkout_date","checkout_time","no_of_children as child","no_of_adults as adult","vehicle_type","vehicle_number as vehicle_registration_no","confirmation_number"], as_dict=1) 
+                data = { k: ('' if v is None else v) for k, v in data.items() }
+                if data["doc_type"] == "Foreigner" or data["doc_type"] == "indianPassport":
+                    data["doc_no"] = data["passport_number"]
+                else:
+                    data["doc_no"] = data["local_id_number"]
+                if data["doc_type"] == "Foreigner" or data["doc_type"] == "indianPassport" or data["doc_type"] == "other":
+                    data["doc_type"] = "Others"
+                elif data["doc_type"] == "aadhaar":
+                    data["doc_type"] = "Aadhaar"
+                elif data["doc_type"] == "driving":
+                    data["doc_type"] = "Driving Licence"
+                elif data["doc_type"] == "voterId":
+                    data["doc_type"] = "Voter_ID"
+                else:
+                    pass
+                data["checkin_date"] = data["checkin_date"].strftime("%b-%m-%Y")
+                data["checkout_date"] = data["checkout_date"].strftime("%b-%m-%Y")
+                data["checkin_time"] = str(data["checkin_time"])
+                data["checkout_time"] = str(data["checkout_time"])
+        
+                first_name = driver.find_elements_by_name("first_name")
+                first_name[0].send_keys(data['first_name'])
 
-        middle_name = driver.find_elements_by_name("middle_name")
-        middle_name[0].send_keys(data['middle_name'])
+                middle_name = driver.find_elements_by_name("middle_name")
+                middle_name[0].send_keys(data['middle_name'])
 
-        last_name = driver.find_elements_by_name("last_name")
-        last_name[0].send_keys(data['last_name'])
+                last_name = driver.find_elements_by_name("last_name")
+                last_name[0].send_keys(data['last_name'])
 
-        company_name = driver.find_elements_by_name("company_name")
-        company_name[0].send_keys(data['company_name'])
+                company_name = driver.find_elements_by_name("company_name")
+                company_name[0].send_keys(data['company_name'])
 
-        house_flat_no = driver.find_elements_by_name("house_flat_no")
-        house_flat_no[0].send_keys(data['house_flat_no'])
+                house_flat_no = driver.find_elements_by_name("house_flat_no")
+                house_flat_no[0].send_keys(data['house_flat_no'])
 
-        address = driver.find_elements_by_name("address")
-        address[0].send_keys(data['address'])
+                address = driver.find_elements_by_name("address")
+                address[0].send_keys(data['address'])
 
-        locality = driver.find_elements_by_name("locality")
-        locality[0].send_keys(data['locality'])
+                locality = driver.find_elements_by_name("locality")
+                locality[0].send_keys(data['locality'])
 
-        city = driver.find_elements_by_name("city")
-        city[0].send_keys(data['city'])
+                city = driver.find_elements_by_name("city")
+                city[0].send_keys(data['city'])
 
-        district = driver.find_elements_by_name("district")
-        district[0].send_keys(data['district'])
+                district = driver.find_elements_by_name("district")
+                district[0].send_keys(data['district'])
 
-        zip_code = driver.find_elements_by_name("zip_code")
-        zip_code[0].send_keys(data['zip_code'])
+                zip_code = driver.find_elements_by_name("zip_code")
+                zip_code[0].send_keys(data['zip_code'])
 
-        country = driver.find_elements_by_name("country")
-        select = Select(country[0])
-        select.select_by_visible_text(data['country'])
+                country = driver.find_elements_by_name("country")
+                select = Select(country[0])
+                select.select_by_visible_text(data['country'])
 
-        time.sleep(2)
-        state = driver.find_elements_by_name("state")
-        select_state = Select(state[0])
-        select_state.select_by_visible_text(data['state'])
+                time.sleep(2)
+                state = driver.find_elements_by_name("state")
+                select_state = Select(state[0])
+                select_state.select_by_visible_text(data['state'])
 
-        # dob = driver.find_elements_by_name("dob")
-        driver.execute_script(
-            f"document.getElementsByName('dob')[0].value  = '{data['dob']}'")
+                # dob = driver.find_elements_by_name("dob")
+                driver.execute_script(
+                    f"document.getElementsByName('dob')[0].value  = '{data['dob']}'")
 
-        mobile_no = driver.find_elements_by_name("mobile_no")
-        mobile_no[0].send_keys(data['mobile_no'])
+                mobile_no = driver.find_elements_by_name("mobile_no")
+                mobile_no[0].send_keys(data['mobile_no'])
 
-        phone_no = driver.find_elements_by_name("phone_no")
-        phone_no[0].send_keys(data['phone_no'])
+                phone_no = driver.find_elements_by_name("phone_no")
+                phone_no[0].send_keys(data['phone_no'])
 
-        email = driver.find_elements_by_name("email")
-        email[0].send_keys(data['email'])
+                email = driver.find_elements_by_name("email")
+                email[0].send_keys(data['email'])
 
-        coming_from = driver.find_elements_by_name("coming_from")
-        coming_from[0].send_keys(data['coming_from'])
+                coming_from = driver.find_elements_by_name("coming_from")
+                coming_from[0].send_keys(data['coming_from'])
 
-        going_to = driver.find_elements_by_name("going_to")
-        going_to[0].send_keys(data['going_to'])
+                going_to = driver.find_elements_by_name("going_to")
+                going_to[0].send_keys(data['going_to'])
 
-        doc_type = driver.find_elements_by_name("doc_type")
-        select_doc_type = Select(doc_type[0])
-        select_doc_type.select_by_visible_text(data['doc_type'])
+                doc_type = driver.find_elements_by_name("doc_type")
+                select_doc_type = Select(doc_type[0])
+                select_doc_type.select_by_visible_text(data['doc_type'])
 
-        doc_no = driver.find_elements_by_name("doc_no")
-        doc_no[0].send_keys(data['doc_no'])
+                doc_no = driver.find_elements_by_name("doc_no")
+                doc_no[0].send_keys(data['doc_no'])
 
-        room_no = driver.find_elements_by_name("room_no")
-        room_no[0].send_keys(data['room_no'])
+                company = frappe.get_last_doc("company")
+                documents = frappe.db.get_list("Guest Details",{"confirmation_number":data["confirmation_number"]},["id_image1"])
+                multi_file = driver.find_element_by_id("customFile")
+                for images in documents:
+                    if "private" in images["id_image1"]:
+                        multi_file.send_keys(folder_path+"/sites/"+company.site_name+images["id_image1"])
+                    else:
+                        multi_file.send_keys(folder_path+"/sites/"+company.site_name+"/public"+images["id_image1"])
+                    
+                room_no = driver.find_elements_by_name("room_no")
+                room_no[0].send_keys(data['room_no'])
 
-        # checkin_date = driver.find_elements_by_name("checkin_date")
-        driver.execute_script(
-            f"document.getElementsByName('checkin_date')[0].value  = '{data['checkin_date']}'")
+                # checkin_date = driver.find_elements_by_name("checkin_date")
+                driver.execute_script(
+                    f"document.getElementsByName('checkin_date')[0].value  = '{data['checkin_date']}'")
 
-        # checkin_time = driver.find_elements_by_name("checkin_time")
-        driver.execute_script(
-            f"document.getElementsByName('checkin_time')[0].value  = '{data['checkin_time']}'")
+                # checkin_time = driver.find_elements_by_name("checkin_time")
+                driver.execute_script(
+                    f"document.getElementsByName('checkin_time')[0].value  = '{data['checkin_time']}'")
 
-        # checkout_date = driver.find_elements_by_name("checkout_date")
-        driver.execute_script(
-            f"document.getElementsByName('checkout_date')[0].value  = '{data['checkout_date']}'")
+                # checkout_date = driver.find_elements_by_name("checkout_date")
+                driver.execute_script(
+                    f"document.getElementsByName('checkout_date')[0].value  = '{data['checkout_date']}'")
 
-        # checkout_time = driver.find_elements_by_name("checkout_time")
-        driver.execute_script(
-            f"document.getElementsByName('checkout_time')[0].value  = '{data['checkout_time']}'")
+                # checkout_time = driver.find_elements_by_name("checkout_time")
+                driver.execute_script(
+                    f"document.getElementsByName('checkout_time')[0].value  = '{data['checkout_time']}'")
 
-        # checkout_time[0].send_keys(data['checkout_time'])
+                # checkout_time[0].send_keys(data['checkout_time'])
 
-        child = driver.find_elements_by_name("child")
-        child[0].send_keys(data['child'])
+                child = driver.find_elements_by_name("child")
+                child[0].send_keys(data['child'])
 
-        adult = driver.find_elements_by_name("adult")
-        adult[0].send_keys(data['adult'])
+                adult = driver.find_elements_by_name("adult")
+                adult[0].send_keys(data['adult'])
 
-        vehicle_type = driver.find_elements_by_name("vehicle_type")
-        select_vehicle_type = Select(vehicle_type[0])
-        select_vehicle_type.select_by_visible_text(data['vehicle_type'])
-        # vehicle_type[0].send_keys(data['vehicle_type'])
+                vehicle_type = driver.find_elements_by_name("vehicle_type")
+                select_vehicle_type = Select(vehicle_type[0])
+                select_vehicle_type.select_by_visible_text(data['vehicle_type'])
+                # vehicle_type[0].send_keys(data['vehicle_type'])
 
-        vehicle_registration_no = driver.find_elements_by_name(
-            "vehicle_registration_no")
-        vehicle_registration_no[0].send_keys(data['vehicle_registration_no'])
+                vehicle_registration_no = driver.find_elements_by_name(
+                    "vehicle_registration_no")
+                vehicle_registration_no[0].send_keys(data['vehicle_registration_no'])
+                guest_doc = frappe.get_doc("Guest Details",each)
+                guest_doc.pathik_status = "In House"
+                guest_doc.pending_pathik = 1
+                guest_doc.save(ignore_permissions=True,ignore_version=True)
+                frappe.db.commit()
+                frappe.publish_realtime("custom_socket", {'message': 'Pathik Upload', 'data': each})
         return {"success":True,"message":"Data uploaded successfully"}
     except Exception as e:
         return {"success":False,"message":str(e)}
