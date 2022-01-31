37f104b0169434cd8018512762d3012e44b9dfbd
prasanthvajja <prasanth@caratred.com>
2022-01-31 18:06:34 +0530

    fixed the issue in reinitiate invoice

--- version2_app/version2_app/doctype/invoices/reinitate_invoice.py
+++ version2_app/version2_app/doctype/invoices/reinitate_invoice.py
@@ -44,6 +44,7 @@ def Reinitiate_invoice(data):
         if "invoice_object_from_file" in data.keys():
             if isinstance(data["invoice_object_from_file"],list):
                 data["invoice_object_from_file"] = ""
+            data["invoice_object_from_file"] = data["invoice_object_from_file"].replace('\" \"',"")
             if data["invoice_object_from_file"] != "":
                 if "data" not in data['invoice_object_from_file'].keys():
                     data['invoice_object_from_file'] = json.dumps({"data":data['invoice_object_from_file']})
