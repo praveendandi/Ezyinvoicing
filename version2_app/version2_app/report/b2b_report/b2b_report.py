6dfe188ede9f2d6fd11ca8194e705b31a638b7f9
pranayalakshmi bhumireddypalli <lakshmi.bhumireddy@caratred.com>
2022-02-22 14:14:01 +0530

    other charges added in b2b report and outward sup report

--- version2_app/version2_app/report/b2b_report/b2b_report.py
+++ version2_app/version2_app/report/b2b_report/b2b_report.py
@@ -11,9 +11,10 @@ def execute(filters=None):
 	try:
 		pd.set_option("display.max_rows", None, "display.max_columns", None)
 		# columns = ["Invoice Number","Invoice Date","Transaction type","Transaction Subtype","Gst Number","Gst Check","Invoice Type","Registered Name","SAC / HSN CODE","Place of Supply (POS)","Taxable Value","Total GST RATE %","IGST Rate","IGST Amount","CGST Rate","CGST Amount","SGST / UT Rate","SGST / UT GST Amount","GST Compensation Cess Rate","GST Compensation Cess Amount","Port Code","Shipping Bill / Bill of Export No.","Shipping Bill / Bill of Export Date","UQC","Quantity","Invoice Cancellation","Pre GST Regime Credit / Debit Note","Original Invoice Number","Original Invoice Date","Original Customer GSTIN / UIN","Original Transaction Type","Reason for issuing Credit / Debit Note","Return Month And Year (MM-YYYY)","Original Invoice Value"]
-		columns = ['Receiver GSTIN / UIN','Receiver Name','Invoice Type (R / SEZ / DE)','Invoice Number','Invoice Date','Invoice Value','Place Of Supply','Reverse Charge','E-Commerce GSTIN','Applicable % of Tax Rate','Tax Rate','Taxable Value','Integrated Tax Amount','Central Tax Amount','State / UT Tax Amount','State Cess Amount','Central Cess Amount',"Irn Generated","Irn Number","Ack No","Ack Date"]
-		fields = ['invoice_number', 'invoice_date','gst_number','trade_name','place_of_supply','invoice_category','total_invoice_amount','sez',"irn_generated","irn_number","ack_no","ack_date"]
+		columns = ['Receiver GSTIN / UIN','Receiver Name','Invoice Type (R / SEZ / DE)','Invoice Number','Invoice Date','Invoice Value','Place Of Supply','Reverse Charge','E-Commerce GSTIN','Applicable % of Tax Rate','Tax Rate','Taxable Value','Integrated Tax Amount','Central Tax Amount','State / UT Tax Amount','State Cess Amount','Central Cess Amount',"Irn Generated","Irn Number","Ack No","Ack Date","Other Charges"]
+		fields = ['invoice_number', 'invoice_date','gst_number','trade_name','place_of_supply','invoice_category','total_invoice_amount','sez',"irn_generated","irn_number","ack_no","ack_date","other_charges"]
 		doc = frappe.db.get_list('Invoices', filters={'invoice_date': ['Between',(filters['from_date'],filters['to_date'])],'irn_generated':['=','Success'],'invoice_category':['!=','Credit Invoice'],'invoice_type':'B2B'},fields=fields,as_list=True)
+		# print(doc)
 		if len(doc) == 0:
 			data = []
 			columns = []
@@ -40,7 +41,7 @@ def execute(filters=None):
 		
 		
 		mergedDf = pd.merge(invoice_df, grouped_df)
-		print(mergedDf.head())
+		# print(mergedDf.head())
 		mergedDf["E-Commerce GSTIN"] = " "
 		# mergedDf["Invoice Type (R / SEZ / DE)"] = " "
 		mergedDf['Applicable % of Tax Rate'] = " "
@@ -53,7 +54,7 @@ def execute(filters=None):
 
 		mergedDf['place_of_supply'] = mergedDf['place_of_supply'].fillna(company.state_code)
 		# mergedDf.loc[(mergedDf.invoice_type=="B2C"),'Gst Check'] = ' '
-		mergedDf.rename(columns={'invoice_number': 'Invoice Number', 'invoice_date': 'Invoice Date','gst_number':'Receiver GSTIN / UIN','trade_name':'Receiver Name','total_invoice_amount':'Invoice Value','place_of_supply':'Place Of Supply','gst_rate':'Tax Rate','item_value':'Taxable Value','igst_amount':'Integrated Tax Amount','cgst_amount':'Central Tax Amount','sgst_amount':'State / UT Tax Amount','state_cess_amount':'State Cess Amount','cess_amount':"Central Cess Amount","irn_generated":"Irn Generated","irn_number":"Irn Number","ack_no":"Ack No","ack_date":"Ack Date"}, inplace=True)
+		mergedDf.rename(columns={"other_charges":"Other Charges",'invoice_number': 'Invoice Number', 'invoice_date': 'Invoice Date','gst_number':'Receiver GSTIN / UIN','trade_name':'Receiver Name','total_invoice_amount':'Invoice Value','place_of_supply':'Place Of Supply','gst_rate':'Tax Rate','item_value':'Taxable Value','igst_amount':'Integrated Tax Amount','cgst_amount':'Central Tax Amount','sgst_amount':'State / UT Tax Amount','state_cess_amount':'State Cess Amount','cess_amount':"Central Cess Amount","irn_generated":"Irn Generated","irn_number":"Irn Number","ack_no":"Ack No","ack_date":"Ack Date"}, inplace=True)
 		mergedDf = mergedDf.sort_values(by=['Invoice Number'])
 		mergedDf = mergedDf[columns]
 		data = mergedDf.values.tolist()
