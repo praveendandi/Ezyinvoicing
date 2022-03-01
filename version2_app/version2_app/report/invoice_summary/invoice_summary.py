c0217ac2fea72efafecfcb66da8a7cf51b1536dd
prasanthvajja <prasanth@caratred.com>
2022-03-01 10:58:32 +0530

    added doctypes

--- version2_app/version2_app/report/invoice_summary/invoice_summary.py
+++ version2_app/version2_app/report/invoice_summary/invoice_summary.py
@@ -3,6 +3,7 @@
 
 from __future__ import unicode_literals
 
+import datetime
 import itertools
 
 import frappe
@@ -22,7 +23,7 @@ def execute(filters=None):
 def get_summary(filters):
     try:
         get_invoices = frappe.db.get_list("Invoices", filters=[['invoice_date', 'between', [
-                                        filters["from_date"], filters["to_date"]]], ["gst_number", "=", filters["gst_number"]]], pluck='name')
+            filters["from_date"], filters["to_date"]]], ["gst_number", "=", filters["gst_number"]]], pluck='name')
         total_items = []
         if len(get_invoices) > 0:
             get_items = frappe.db.get_list(
@@ -40,15 +41,19 @@ def get_summary(filters):
                     sac_category = frappe.db.get_value(
                         "SAC HSN CODES", {"sac_index": each.sac_index}, "category")
                     each["service_type"] = sac_category
-                each["Date_string"] = str(
-                    min(get_item_dates))+" to "+str(max(get_item_dates))
+                # start_date = datetime.datetime.strptime(str(min(get_item_dates)),'%Y-%m-%d').strftime("%d %B %Y")
+                start_date = min(get_item_dates).strftime("%d %B %Y")
+                end_date = max(get_item_dates).strftime("%d %B %Y")
+                each["Date_string"] = start_date+" to "+end_date
                 total_items.append(each)
             df = pd.DataFrame.from_records(total_items)
-            data = df.groupby('service_type',as_index=False).agg({"Date_string":'first', "service_type": 'first', "invoice_category": 'first', "item_value_after_gst": 'sum'})
-            data.rename(columns={'Date_string': 'Date', 'service_type': 'Service Type','invoice_category':'Invoice Type','item_value_after_gst':'Amount'}, inplace=True)
+            data = df.groupby('service_type', as_index=False).agg(
+                {"Date_string": 'first', "service_type": 'first', "invoice_category": 'first', "item_value_after_gst": 'sum'})
+            data.rename(columns={'Date_string': 'Date', 'service_type': 'Service Type',
+                        'invoice_category': 'Invoice Type', 'item_value_after_gst': 'Amount'}, inplace=True)
             data = data.values.tolist()
             columns = ["Date", "Service Type", "Invoice Type", "Amount"]
-            return {"success":True, "data":data, "columns":columns}
-        return {"success":False, "message":"no data found"}
+            return {"success": True, "data": data, "columns": columns}
+        return {"success": False, "message": "no data found"}
     except Exception as e:
-        return {"success":False, "message": str(e)}
+        return {"success": False, "message": str(e)}
