854d7c4c3fa580bbb6295b7c9b4d730aec8ba5b5
vineetha <vineetha.s@caratred.com>
2022-07-13 12:30:45 +0530

    created new module for Recon

---
+++ version2_app/reconciliation/doctype/converted_b2c_to_b2b/converted_b2c_to_b2b.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, caratred and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Converted B2C to B2B', {
+	// refresh: function(frm) {
+
+	// }
+});
