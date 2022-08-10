bb240d2949a7af2230cf9e130b0a0374e8d10538
ganureddy54 <ganureddy54@gmail.com>
2022-08-10 15:53:36 +0530

    b2c reupload bulkupload lalit

--- version2_app/version2_app/doctype/invoices/bulkupload.py
+++ version2_app/version2_app/doctype/invoices/bulkupload.py
@@ -13,6 +13,7 @@ def bulkupload(data):
     try:
         invoice_data=data
         company =invoice_data['company']
+        print(company,"/////////////////")
         start_time = datetime.datetime.now()
         items=[]
         companyData = frappe.get_doc('company',company)#data['company'])
@@ -162,7 +163,7 @@ def bulkupload(data):
                 if inv_data.docstatus!=2 and inv_data.irn_generated!="Success" and inv_data.invoice_type=="B2B":
                     reupload = True
                 elif inv_data.invoice_type == "B2C":
-                    if inv_data.irn_generated=="Success" and company.company_code in ['LAAB-01','LAAB-01','LAKOL-01','LAMU-01','LGPS-01','LTVK-01','LABE-01','LAGO-01','LAJA-01','LAMAN-01','TLND-01','TALU-01']:
+                    if inv_data.irn_generated=="Success" and company in ['LAAB-01','LAAB-01','LAKOL-01','LAMU-01','LGPS-01','LTVK-01','LABE-01','LAGO-01','LAJA-01','LAMAN-01','TLND-01','TALU-01']:
                         reupload = False
                     else:
                         reupload = True
