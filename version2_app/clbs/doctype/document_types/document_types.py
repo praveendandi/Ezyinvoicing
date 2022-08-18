140edde12d9680cf28f43b9c3d79feb445578a63
prasanth vajja <prasanth@caratred.com>
2022-08-18 14:07:09 +0530

    Revert "Merge branch 'document_sequence_clbs' into 'Merge_Branches'"
    This reverts merge request !160

--- version2_app/clbs/doctype/document_types/document_types.py
+++ version2_app/clbs/doctype/document_types/document_types.py
@@ -3,37 +3,8 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
-import json
+# import frappe
 from frappe.model.document import Document
 
 class DocumentTypes(Document):
 	pass
-
-
-def add_document_sequence(doc, method=None):
-    try:
-        get_count = frappe.db.count('Document Types')
-        get_count = get_count+1
-        doc.sequence = get_count
-    except Exception as e:
-        frappe.log_error(str(e), "add_document_sequence")
-        return {"Success":False,"message":str(e)}
-    
-@frappe.whitelist(allow_guest=True)
-def update_document_sequence(data):
-    try:
-        # print(data,"................")
-        # if isinstance(data, str):
-        #     print("......................")
-        #     data = json.loads(data)
-        if len(data["data"]) > 0:
-            for each in data["data"]:
-                print(each,"..................")
-                frappe.db.set_value("Document Types", each["name"], "sequence", each["sequence"])
-                frappe.db.commit()
-            return {"success": True, "message": "Sequence updated"}
-        return {"success": False, "message": "No data found"}
-    except Exception as e:
-        frappe.log_error(str(e), "add_document_sequence")
-        return {"Success":False,"message":str(e)}
