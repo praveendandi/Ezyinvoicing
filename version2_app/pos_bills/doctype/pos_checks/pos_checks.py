a8225a5851278f74ff6b2e482a0527cc49ab6bc9
shrikant <shrikant.b@caratred.com>
2022-09-13 18:54:00 +0530

    handled line items in zone park

--- version2_app/pos_bills/doctype/pos_checks/pos_checks.py
+++ version2_app/pos_bills/doctype/pos_checks/pos_checks.py
@@ -143,6 +143,8 @@ def extract_data(payload,company_doc):
         data={}
         raw_data = payload.split("\n")
         total_amount = ""
+        check_date = ""
+        check_number = ""
         for line in raw_data:
             if company_doc.closed_check_reference in payload and company_doc.void_check_reference not in payload:
                 data["check_type"] = "Check Closed"
@@ -191,6 +193,7 @@ def extract_data(payload,company_doc):
                     check_string = check_regex[0] if len(check_regex)>0 else ""
                     check_no_regex = re.findall("\w+\d+",check_string)
                     data["check_no"] = check_no_regex[0] if len(check_no_regex) > 0 else ""
+                    check_number = check_no_regex[0] if len(check_no_regex) > 0 else ""
                 if company_doc.table_number_reference in line and "GSTIN" not in line and "GST IN" not in line:
                     table_regex = re.findall(company_doc.table_number_regex, line.strip())
                     table_string = table_regex[0] if len(table_regex)>0 else ""
@@ -210,6 +213,7 @@ def extract_data(payload,company_doc):
                         checkdate = check_date[0] if len(check_date) > 0 else ""
                         if checkdate != "":
                             data["check_date"] = datetime.strptime(checkdate,company_doc.pos_check_date_format).strftime('%Y-%m-%d')
+                        check_date = datetime.strptime(checkdate,company_doc.pos_check_date_format).strftime('%y%m%d')
                         # data["check_date"] = format_date(
                         #     checkdate,
                         #     "yyyy-mm-dd",
@@ -218,6 +222,9 @@ def extract_data(payload,company_doc):
                         # print("Today date is: ", today)
                         # if today > data["check_date"]:
                         #     pass
+        if company_doc.name == "ZBPK-01":
+            if check_date != "" and check_number != "":
+                data["check_no"] = check_date+check_number
         data["total_amount"] = str(total_amount)
         return {"success":True, "data":data}
     except Exception as e:
