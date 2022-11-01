416d3962f4a32471d9150f646ff110c368fc06d1
phanindra <k.phanindra@caratred.com>
2022-11-01 09:59:14 +0530

    digital signature with Date,Company,Hotel name,Location

--- version2_app/e_signature/e_signature.py
+++ version2_app/e_signature/e_signature.py
@@ -13,15 +13,18 @@ from pyhanko.sign import signers, fields, timestamps
 from pyhanko import stamp
 from pyhanko.pdf_utils.font import opentype
 from pyhanko.pdf_utils import text, images
+from version2_app.utils import html_to_pdf, convert_image_to_base64
 
 
 class UserSignature(Document):
     pass
 
 
-@frappe.whitelist(allow_guest=True)
-def add_signature(invoice=None, pfx_signature=None, signature_image=None, secret=b'123', X1=400, Y1=10, X2=590, Y2=70, company=None, summary=None):
+@frappe.whitelist()
+def add_signature(invoice=None, pfx_signature=None, signature_image=None, secret=None, X1=400, Y1=10, X2=590, Y2=70, company=None, summary=None):
     try:
+        company_data = frappe.get_doc('company')
+        print(company_data,'////////////')
         if secret:
             secret = bytes(secret, 'utf-8')
         site_name = cstr(frappe.local.site)
@@ -31,6 +34,7 @@ def add_signature(invoice=None, pfx_signature=None, signature_image=None, secret
             + "/sites/"
             + site_name
         )
+        company_data = frappe.get_doc('company')
         signer = signers.SimpleSigner.load_pkcs12(
             # pfx_file='/home/caratred/Desktop/projects/pyhanko/sign.pfx', passphrase=b'secret'
             pfx_file=invoice_file+pfx_signature, passphrase=secret
@@ -41,12 +45,23 @@ def add_signature(invoice=None, pfx_signature=None, signature_image=None, secret
             use_pades_lta=True
         )
         pdf_signer = signers.PdfSigner(
-            signature_meta, signer=signer, stamp_style=stamp.TextStampStyle(
-                # the 'signer' and 'ts' parameters will be interpolated by pyHanko, if present
-                stamp_text='\n\n\n\nTime: %(ts)s',
-                background=images.PdfImage(invoice_file+signature_image)
+        signature_meta, signer=signer, stamp_style=stamp.TextStampStyle(
+            # the 'signer' and 'ts' parameters will be interpolated by pyHanko, if present
+            # stamp_text='\n\n\nTime: %(ts)s',
+            # stamp_text='Signed by: %(signer)s\nTime: %(ts)s',
+            stamp_text='Signed by: '+company_data.company+'\nHotel Name: '+company_data.legal_name+'\nLocation: '+company_data.location+'\nTime: %(ts)s',
+            # background=images.PdfImage(
+            #     stamp_image_path),
+            # border_width=1,
+            text_box_style=text.TextBoxStyle(
+                border_width=0
+                # font=opentype.GlyphAccumulatorFactory('path/to/NotoSans-Regular.ttf')
             ),
-        )
+            # box=None
+        ),
+    )
+        return pdf_signer
+
         file_name = os.path.basename(invoice)
         output_file_path = invoice_file+"/public/files/"+file_name
         with open(invoice_file+invoice, 'rb') as inf:
@@ -110,3 +125,73 @@ def send_files(files, user_name, summary):
     except Exception as e:
         frappe.log_error(str(e), "send_files")
         return{"success": False, "message": str(e)}
+
+
+@frappe.whitelist()
+def add_esignature_to_invoice(invoice_number=None, based_on="user", etax=None, type="invoice"):
+    try:
+        company = frappe.get_last_doc("company")
+        if type == "etax":
+            get_value = etax
+        else:
+            get_value = frappe.db.get_value("Invoices", invoice_number, ["invoice_file"])
+        if not frappe.db.exists("CLBS Settings", company.name):
+            return {"success": False, "message": "Add signature coordinates in settings"}
+        if company.e_signature == "User":
+            user_name = frappe.session.user
+            if not frappe.db.exists("User Signature", user_name):
+                return {"success": False, "message": "add your signature in user settings"}
+            coordinates = frappe.db.get_value("CLBS Settings", company.name, ["X1", "Y1", "X2", "Y2"], as_dict=True)
+            user_signature = frappe.db.get_value("User Signature", user_name, ["signature_image","signature_pfx","pfx_password"],as_dict=True)
+            add_sig = add_signature(invoice=get_value, pfx_signature=user_signature["signature_pfx"], signature_image=user_signature["signature_image"], secret=user_signature["pfx_password"], X1=coordinates["X1"], Y1=coordinates["Y1"], X2=coordinates["X2"], Y2=coordinates["Y2"], company=company, summary=invoice_number)
+            return add_sig
+        elif company.e_signature == "Organization":
+            coordinates = frappe.db.get_value("CLBS Settings", company.name, ["X1", "Y1", "X2", "Y2"], as_dict=True)
+            add_sig = add_signature(invoice=get_value, pfx_signature=company.signature_pfx, signature_image=company.signature_image, secret=company.pfx_password, X1=coordinates["X1"], Y1=coordinates["Y1"], X2=coordinates["X2"], Y2=coordinates["Y2"], company=company, summary=invoice_number)
+            return add_sig
+        else:
+            return {"success": False, "message": "digital signature is in disable mode"}
+    except Exception as e:
+        frappe.log_error(str(e), "add_esignature_to_invoice")
+        return{"success": False, "message": str(e)}
+
+
+@frappe.whitelist(allow_guest=True)
+def add_signature_on_etax(invoice_number=None):
+    try:
+        company = frappe.get_last_doc("company")
+        if not frappe.db.exists("Invoices", invoice_number):
+            return {"success": False, "message": "invoice not found"} 
+        doc = frappe.get_doc("Invoices", invoice_number)
+        qr_image_base = ""
+        company_logo_base = ""
+        if company.company_logo:
+            convimgtobase = convert_image_to_base64(company.company_logo)
+            if not convimgtobase["success"]:
+                return convimgtobase
+            company_logo_base = "data:image/png;base64,"+convimgtobase["data"]
+        if doc.qr_code_image:
+            convimgtobase = convert_image_to_base64(
+                doc.qr_code_image)
+            if not convimgtobase["success"]:
+                return convimgtobase
+            qr_image_base = "data:image/png;base64," + \
+                convimgtobase["data"]
+        templates = frappe.db.get_value("Print Format", {"name": "E-Tax Invoice"}, ["html"])
+        if not templates:
+            return {"success": False, ",message": "please add print formats"}
+        data = doc.as_dict()
+        data["qr_code_image"] = qr_image_base
+        data["company_logo_base"] = company_logo_base
+        html_data = frappe.render_template(templates, data)
+        convert_html_to_pdf = html_to_pdf(
+            html_data, "ETax-Invoice-", invoice_number)
+        if not convert_html_to_pdf["success"]:
+            return convert_html_to_pdf
+        # return convert_html_to_pdf
+        etax_qr = add_esignature_to_invoice(invoice_number=invoice_number, etax=convert_html_to_pdf["file_url"], type="etax")
+        return etax_qr
+    except Exception as e:
+        frappe.log_error(str(e), "add_esignature_to_invoice")
+        return{"success": False, "message": str(e)}
+
