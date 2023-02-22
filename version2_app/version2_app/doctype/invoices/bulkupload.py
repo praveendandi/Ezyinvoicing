e55a1ef9f3a758f9d0c406aa2a657ba72f91a2cd
vineetha <vineetha.s@caratred.com>
2023-02-22 13:12:51 +0530

    add invoice no prefix in bulkupload

--- version2_app/version2_app/doctype/invoices/bulkupload.py
+++ version2_app/version2_app/doctype/invoices/bulkupload.py
@@ -60,6 +60,8 @@ def bulkupload(data):
                 item[bulk_meta_data["Gst_details"]["invoice_number"]] = "1-" + str(item[bulk_meta_data["Gst_details"]["invoice_number"]])
             if invoice_data["company"]=="FBMC-01":
                 item[bulk_meta_data["Gst_details"]["invoice_number"]] = "2--" + str(item[bulk_meta_data["Gst_details"]["invoice_number"]])
+            if invoice_data["company"]=="GMB-01":
+                item[bulk_meta_data["Gst_details"]["invoice_number"]] = "GMB-" + str(item[bulk_meta_data["Gst_details"]["invoice_number"]])
             if invoice_data["company"]=="RCP-01":
                 item[bulk_meta_data["Gst_details"]["invoice_number"]] = str(item[bulk_meta_data["Gst_details"]["invoice_number"]]).replace("7392","")
                 item[bulk_meta_data["Gst_details"]["invoice_number"]] = "7392-" + str(item[bulk_meta_data["Gst_details"]["invoice_number"]])
@@ -99,6 +101,8 @@ def bulkupload(data):
             #     item[bulk_meta_data["Gst_details"]["invoice_number"]] = str(item[bulk_meta_data["Gst_details"]["invoice_number"]]).lstrip("0")[1:]
             if invoice_data["company"]=="JMR-01":
                 each[bulk_meta_data["detail_folio"]["invoice_number"]] = "1-" + each[bulk_meta_data["detail_folio"]["invoice_number"]]
+            if invoice_data["company"]=="GMB-01":
+                each[bulk_meta_data["detail_folio"]["invoice_number"]] = "GMB-" + each[bulk_meta_data["detail_folio"]["invoice_number"]]
             if invoice_data["company"]=="FBMC-01":
                 each[bulk_meta_data["detail_folio"]["invoice_number"]] = "2--" + each[bulk_meta_data["detail_folio"]["invoice_number"]]
             if invoice_data["company"]=="RCP-01":
