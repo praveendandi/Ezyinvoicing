4bc8a1206a83a9e58db55091db1a9d145a740d91
sumanth <sumanth>
2023-02-23 15:44:14 +0530

    added prod irn api for ey intigration

--- version2_app/version2_app/doctype/ey_intigration/api_urls.py
+++ version2_app/version2_app/doctype/ey_intigration/api_urls.py
@@ -8,11 +8,11 @@ prod_login = 'https://eapi.eyasp.com/eapi/v2.0/authenticate'
 prod_refresh_token = 'https://eapi.eyasp.com/eapi/v2.0/refreshtoken'
 
 test_irn = 'https://einvoicesyncapi.eyasp.in/einvoiceapi/v1.0/generateExelIRN'
-prod_irn = 'https://eapi.eyasp.com/eapi/v2.0/cancelIRN'
+prod_irn = 'https://eapi.eyasp.com/eapi/v1.0/generateExelIRN'
 
 
 test_cancel_irn = 'https://einvoicesyncapi.eyasp.in/einvoiceapi/v2.0/cancelIRN'
-prod_cancel_irn = 'https://einvoicesyncapi.eyasp.in/einvoiceapi/v2.0/cancelIRN'
+prod_cancel_irn = 'https://eapi.eyasp.com/eapi/v2.0/cancelIRN'
 
 test_redo_qr = 'https://einvoicesyncapi.eyasp.in/einvoiceapi/v2.0/findIRN'
 prod_redo_qr = 'https://eapi.eyasp.com/eapi/v2.0/findIRN'
