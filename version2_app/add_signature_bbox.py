c08f453a299d3b9f1bd9f4fb5bde567f2f16b0c1
vineetha <vineetha.s@caratred.com>
2023-03-14 14:45:41 +0530

    minor changes in bbox event

--- version2_app/add_signature_bbox.py
+++ version2_app/add_signature_bbox.py
@@ -1,31 +1,36 @@
 import frappe
 import pdfplumber
+import sys, traceback
 
 
 def create_bbox(doc,method=None):
-    folder_path = frappe.utils.get_bench_path()
-    company = frappe.get_doc('company', doc.company)
-    site_folder_path = company.site_name
-    file_path = folder_path+'/sites/'+site_folder_path+doc.invoice_file
+    try:
+        if doc.invoice_from in ["Web", "Pms"]:
+            folder_path = frappe.utils.get_bench_path()
+            company = frappe.get_doc('company', doc.company)
+            site_folder_path = company.site_name
+            file_path = folder_path+'/sites/'+site_folder_path+doc.invoice_file
 
-    print(file_path)
-    x0,x1,top, bottom = '','','',''
+            x0,x1,top, bottom = '','','',''
 
-    with pdfplumber.open(file_path) as pdf:
-        count = len(pdf.pages)
-        for index in range(count):
-            first_page = pdf.pages[index]
-            for word in first_page.extract_words():
-                if word['text'] in ['Signature','signature','SIGNATURE']:
-                    print(word)
-                    x0 = word['x0']
-                    x1 = word['x1']
-                    top = word['top']
-                    bottom = word['bottom']
-    doc.x0 = x0
-    doc.x1 = x1
-    doc.top = top
-    doc.bottom = bottom
+            with pdfplumber.open(file_path) as pdf:
+                count = len(pdf.pages)
+                for index in range(count):
+                    first_page = pdf.pages[index]
+                    for word in first_page.extract_words():
+                        if word['text'] in ['Signature','signature','SIGNATURE']:
+                            print(word)
+                            x0 = word['x0']
+                            x1 = word['x1']
+                            top = word['top']
+                            bottom = word['bottom']
+            doc.x0 = x0
+            doc.x1 = x1
+            doc.top = top
+            doc.bottom = bottom
 
-    doc.save()
-
+            doc.save()
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        frappe.log_error("Ezy-invoicing manualTax_credit_to_debit","line No:{}\n{}".format(exc_tb.tb_lineno,traceback.format_exc()))
+        return {"success": False, "message": str(e)}
