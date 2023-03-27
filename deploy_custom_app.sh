36b165e605d2acc0c97c8d1eadb23e2824cd2d78
arvind <arvind@caratred.com>
2023-03-27 18:11:10 +0530

    Update deploy_custom_app.sh

--- deploy_custom_app.sh
+++ deploy_custom_app.sh
@@ -2,18 +2,16 @@
 WORKDIR=/home/erpnext/bench/frappe-bench/
 app1=version2_app
 app2=invoice_sync
-app3=hrms
 app1_repo_url=https://gitlab-ci-token:glpat-yk-_nkFvkGysxbYUevnz@gitlab.caratred.com/ganesh.s/EzyinvoiceDemo.git
 app2_repo_url=https://gitlab-ci-token:glpat-hkCE2pJqAC4ywuTHD4wP@gitlab.caratred.com/sumanth512/invoice-sync.git
-app3_repo_url=https://github.com/frappe/hrms.git
 
 # Check if the first app exists
 
-if [ -d "${WORKDIR}/apps/$app1" ]
+if [ -d "${WORKDIR}/apps/${app1}" ]
 then
   echo "Updating $app1"
   # Change directory to the app directory
-  cd "${WORKDIR}/apps/$app1"
+  cd "${WORKDIR}/apps/${app1}"
   #git remote remove origin
   #git remote remove upstream
 
@@ -34,15 +32,12 @@ else
   bench --site lifescc install-app $app1
 fi
 
-cd $WORKDIR
 # Check if the second app exists
-if [ -d "${WORKDIR}/apps/$app2" ]
+if [ -d "${WORKDIR}/apps/${app2}" ]
 then
   echo "Updating $app2"
   # Change directory to the app directory
-  cd "${WORKDIR}/apps/$app2"
-  # Pull the latest changes from the git repository
-  git pull
+  cd "${WORKDIR}/apps/${app2}"
   # Get the latest tag for the branch and update to it
   git fetch --tags
   git checkout $(git describe --tags $(git rev-list --tags --max-count=1))
@@ -55,7 +50,11 @@ else
   # Install the app in the given site
   bench --site lifescc install-app $app2
 fi
-cd $WORKDIR
+
+# Once all the apps are updated, migrate the database and requirements
+bench migrate
+bench update --requirements
+
 # Check if the third app exists
 if [ -d "frappe-bench/apps/$app3" ]
 then
@@ -76,3 +75,4 @@ else
   # Install the app in the given site
   bench --site lifescc install-app $app3
 fi
+done
