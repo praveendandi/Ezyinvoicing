1ae78ed041dedc26b20e0535af5c185a45ff4700
arvind <arvind@caratred.com>
2023-03-28 18:12:41 +0530

    Update deploy_custom_app.sh

--- deploy_custom_app.sh
+++ deploy_custom_app.sh
@@ -11,52 +11,53 @@ APP2_REPO_URL=https://gitlab-ci-token:glpat-hkCE2pJqAC4ywuTHD4wP@gitlab.caratred
 # Check if the first app exists
 
 if [ -d "$WORKDIR/apps/$APP1" ]; then
-  
+
   echo "Updating $APP1"
   # Change directory to the app directory
   cd "$WORKDIR/apps/$APP1"
+
    # git remote remove origin
    # git remote remove upstream
 
    # git remote add origin https://gitlab-ci-token:glpat-yk-_nkFvkGysxbYUevnz@gitlab.caratred.com/ganesh.s/EzyinvoiceDemo.git
    # git remote add upstream https://gitlab-ci-token:glpat-yk-_nkFvkGysxbYUevnz@gitlab.caratred.com/ganesh.s/EzyinvoiceDemo.git
 
-# Pull the latest changes from the git repository
-# Step 2: Check the git branch
-    pwd
-  if [[ "$(git rev-parse --abbrev-ref --short HEAD)" == "master" ]]; then
-    # Check for latest tag with prefix stable
-    tag=$(git describe --tags --match "stable-*" --abbrev=0)
-    if [[ "$tag" == "" ]]; then
-        echo "No stable tag found"
-    else
-        # Checkout to stable prefix tag and migrate the site
-        git checkout "$tag"
-        bench migrate
-    fi
-else
-    # Check if git branch is detached tag
-    if [[ "$(git describe --tags)" != "" ]]; then
-        # Checkout to master branch
-        git checkout master
-        # Check for latest tag with prefix stable
-        tag=$(git describe --tags --match "stable-*" --abbrev=0)
-        if [[ "$tag" == "" ]]; then
-            echo "No stable tag found"
-        else
-            # Checkout to stable prefix tag and migrate the site
-            git checkout "$tag"
-            bench migrate
+  # Pull the latest changes from the git repository
+  # Check if the current branch is master
+        if [[ "$(git rev-parse --abbrev-ref HEAD)"=="master" ]]; then
+        # Find the latest tag with the prefix "stable"
+        git fetch --tag
+        latest_stable_tag=$(git describe --abbrev=0 --tags --match "stable*")
+
+        # Check if a stable tag was found
+        if [ -n "$latest_stable_tag" ]; then
+                # Checkout the latest stable tag
+                git checkout "$latest_stable_tag"
+                 echo "Checked out tag: $latest_stable_tag"
+         else
+                echo "No stable tags found"
         fi
-    else
-        echo "No stable tag found"
-    fi
-fi
-  cd ../../..
+        else
+        # Checkout the master branch
+        git checkout master
+
+        # Find the latest tag with the prefix "stable"
+        git fetch --tag
+        latest_stable_tag=$(git describe --abbrev=0 --tags --match "stable*")
+
+        # Check if a stable tag was found
+            if [ -n "$latest_stable_tag" ]; then
+                # Checkout the latest stable tag
+                git checkout "$latest_stable_tag"
+                echo "Checked out tag: $latest_stable_tag"
+            else
+                echo "No stable tags found"
+            fi
+        fi
 else
-  cd $WORKDIR
-  # If the app does not exist, get it from the git repository
-  echo "Installing $APP1"
-  bench get-app $APP1_REPO_URL  # Install the app in the given site
-  bench --site $SITE_NAME install-app $APP1
+          cd $WORKDIR
+          # If the app does not exist, get it from the git repository
+          echo "Installing $APP1"
+          bench get-app $APP1_REPO_URL  # Install the app in the given site
+          bench --site $SITE_NAME install-app $APP1
 fi
