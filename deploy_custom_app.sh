edfacaa575fab1cad969e4b1834895a9ebf0a2b3
arvind <arvind@caratred.com>
2023-03-28 16:16:25 +0530

    Update deploy_custom_app.sh

--- deploy_custom_app.sh
+++ deploy_custom_app.sh
@@ -16,48 +16,52 @@ then
   echo "Updating $APP1"
   # Change directory to the app directory
   cd "$WORKDIR/apps/$APP1"
-   # git remote remove origin
-   # git remote remove upstream
+   git remote remove origin
+   git remote remove upstream
 
-   # git remote add origin https://gitlab-ci-token:glpat-yk-_nkFvkGysxbYUevnz@gitlab.caratred.com/ganesh.s/EzyinvoiceDemo.git
-   # git remote add upstream https://gitlab-ci-token:glpat-yk-_nkFvkGysxbYUevnz@gitlab.caratred.com/ganesh.s/EzyinvoiceDemo.git
+   git remote add origin https://gitlab-ci-token:glpat-yk-_nkFvkGysxbYUevnz@gitlab.caratred.com/ganesh.s/EzyinvoiceDemo.git
+   git remote add upstream https://gitlab-ci-token:glpat-yk-_nkFvkGysxbYUevnz@gitlab.caratred.com/ganesh.s/EzyinvoiceDemo.git
 
 # Pull the latest changes from the git repository
-# Get the latest tag for the branch and update to it
-# Check current branch
+# Get the current branch or tag
+
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-echo $CURRENT_BRANCH
+CURRENT_TAG=$(git describe --exact-match --tags HEAD 2>/dev/null)
 
-if [ "$CURRENT_BRANCH" == "master" ]; then
-  # Check for latest stable tag with prefix
-  STABLE_TAG=$(git describe --tags --match "$STABLE_TAG_PREFIX*" --abbrev=0)
-
-  # Checkout to latest stable tag with prefix
-  git checkout "$STABLE_TAG"
-
+if [ -z "$CURRENT_TAG" ]; then
+  # Current branch is not a tag
+  if [ "$CURRENT_BRANCH" = "master" ]; then
+    # If the current branch is master, check out the latest stable tag
+    LATEST_TAG=$(git describe --tags --match "$STABLE_TAG_PREFIX*" --abbrev=0)
+    git checkout "$LATEST_TAG"
+  else
+    # Otherwise, check out the master branch
+    git checkout master
+    # Check for the latest stable tag and check out to it
+    LATEST_TAG=$(git describe --tags --match "$STABLE_TAG_PREFIX*" --abbrev=0)
+    git checkout "$LATEST_TAG"
+  fi
 else
-  # Checkout to master branch
-  git checkout master
-
-  # Check for latest stable tag with prefix
-  STABLE_TAG=$(git describe --tags --match "$STABLE_TAG_PREFIX*" --abbrev=0)
-
-  # Checkout to latest stable tag with prefix
-  git checkout "$STABLE_TAG"
+  # Current branch is a tag
+  git checkout "$CURRENT_TAG"
+  # Check for the latest stable tag and check out to it
+  LATEST_TAG=$(git describe --tags --match "$STABLE_TAG_PREFIX*" --abbrev=0)
+  git checkout "$LATEST_TAG"
 fi
 
-# Migrate site
-bench --site "$SITE_NAME" migrate
+# Migrate the site
+bench migrate
 
-if [ $? -eq 0 ]; then
-  echo "Build success and deploy"
+# Check if the migration was successful
+if [ "$?" -eq 0 ]; then
+  echo "Build success"
 else
-  # Check out to last successful tag branch
-  LAST_SUCCESS_TAG=$(git describe --tags --match "success-*" --abbrev=0)
-  git checkout "$LAST_SUCCESS_TAG"
-
-  # Migrate site again
-  bench --site "$SITE_NAME" migrate
+  # If migration failed, revert tag branch to previous state
+  git reset --hard HEAD@{1}
+  # Check out to the previous tag
+  PREV_TAG=$(git describe --tags --abbrev=0 "$LATEST_TAG"^)
+  git checkout "$PREV_TAG"
+  bench migrate
 fi
 else
   # # Change directory back to the frappe-bench directory
@@ -68,3 +72,75 @@ else
   # Install the app in the given site
   bench --site $SITE_NAME install-app $APP1
 fi
+
+
+
+# # Check if the second app exists
+
+# if [ -d "${WORKDIR}/apps/${APP2}" ]
+# then
+#   echo "Updating $APP2"
+#   # Change directory to the app directory
+#   cd "${WORKDIR}/apps/${APP2}"
+#    # git remote remove origin
+#    # git remote remove upstream
+
+#    # git remote add origin https://gitlab-ci-token:glpat-yk-_nkFvkGysxbYUevnz@gitlab.caratred.com/ganesh.s/EzyinvoiceDemo.git
+#    # git remote add upstream https://gitlab-ci-token:glpat-yk-_nkFvkGysxbYUevnz@gitlab.caratred.com/ganesh.s/EzyinvoiceDemo.git
+
+# # Pull the latest changes from the git repository
+# # Get the latest tag for the branch and update to it
+# # Get the name of the current branch
+# CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+# echo $CURRENT_BRANCH
+
+# # Check if we're on the master branch
+# if [ "$CURRENT_BRANCH" == "master" ]; then
+#   # Check for a stable tag with the prefix
+#   STABLE_TAG=$(git tag -l "$STABLE_PREFIX*" | sort -V | tail -n1)
+#   if [ -n "$STABLE_TAG" ]; then
+#     # Checkout the latest stable tag with the prefix
+#     git checkout "$STABLE_TAG"
+#   echo $STABLE_TAG checkout successfull
+#   else
+#   echo $STABLE_TAG not found.
+#   fi
+# else
+#   echo CURRENT_BRANCH is not "master"
+#   # Checkout the master branch
+#   git checkout master
+#   # Check for a stable tag with the prefix
+#   STABLE_TAG=$(git tag -l "$STABLE_PREFIX*" | sort -V | tail -n1)
+#   if [ -n "$STABLE_TAG" ]; then
+#     # Checkout the latest stable tag with the prefix
+#     echo $STABLE_TAG
+#     git checkout "$STABLE_TAG"
+#     else
+#     echo $STABLE_TAG not found.
+#   fi
+# fi
+
+# # Migrate the site
+# bench use $SITE_NAME
+# if bench migrate; then
+#   # If the migration succeeds, echo "build success" and deployed
+#   echo "build success"
+#   # TODO: Deploy the site
+# else
+#   # If the migration fails, checkout the previous successful tag branch and migrate again
+#   LAST_SUCCESS_TAG=$(git tag --list --merged HEAD "success-*" | sort -V | tail -n1)
+#   if [ -n "$LAST_SUCCESS_TAG" ]; then
+#     git checkout "$LAST_SUCCESS_TAG"
+#     bench migrate
+#     # TODO: Deploy the site
+# else
+#   # # Change directory back to the frappe-bench directory
+#   cd ${WORKDIR}
+#   # If the app does not exist, get it from the git repository
+#   echo "Installing $APP2"
+#   bench get-app ${APP2_REPO_URL}
+#   # Install the app in the given site
+#   bench --site ${SITE_NAME} install-app ${APP2}
+# fi
+#   fi
+# fi
