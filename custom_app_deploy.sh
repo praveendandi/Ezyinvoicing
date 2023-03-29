def990890739b91cea0e58bfc78649055a695428
arvind <arvind@caratred.com>
2023-03-30 01:02:37 +0530

    Update custom_app_deploy.sh tag_prefix not found and bench migrate failed

--- custom_app_deploy.sh
+++ custom_app_deploy.sh
@@ -23,22 +23,32 @@ if [ "$current_branch" == "master" ]; then
   # If the current branch is master, fetch the latest tag with the prefix and checkout to it
   git fetch --tags
   latest_tag=$(git describe --tags --match "${tag_prefix}*" `git rev-list --tags --max-count=1`)
-  git checkout $latest_tag
+  if [ -n "$latest_tag" ]; then
+    git checkout $latest_tag
+  else
+    echo "$tag_prefix not found"
+    exit 1
+  fi
 else
   # If the current branch is not master, checkout to it and fetch the latest tag with the prefix
   git checkout master
   git fetch --tags
   latest_tag=$(git describe --tags --match "${tag_prefix}*" `git rev-list --tags --max-count=1`)
-  git checkout $latest_tag
+  if [ -n "$latest_tag" ]; then
+    git checkout $latest_tag
+  else
+    echo "$tag_prefix not found"
+    exit 1
+  fi 
 fi
 
 # Migrate the site and set up requirements
 bench --site $site_name migrate
-bench setup requirements
 
 # Check if the migration was successful
 if [ $? -eq 0 ]; then
   echo "$custom_app updated successfully"
+  bench setup requirements
 else
   # If the migration failed, checkout the previous tag branch
   git checkout $current_branch
