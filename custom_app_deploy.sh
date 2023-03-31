c23c2312d689d7ec26122c3801e94a9c5681db63
arvind <arvind@caratred.com>
2023-03-30 23:43:29 -0700

    custom_app_deploy modified site for backups

--- custom_app_deploy.sh
+++ custom_app_deploy.sh
@@ -1,57 +1,78 @@
 #!/bin/bash
 
 # Define variables
-custom_app=version2_app
-git_url=https://gitlab-ci-token:glpat-yk-_nkFvkGysxbYUevnz@gitlab.caratred.com/ganesh.s/EzyinvoiceDemo.git
-site_name=ezyinvoicing.local
-tag_prefix=stable
-branch_name=Merge_Branches
+APP_NAME="version2_app"
+BKP_DIR_TO_COPY="~/DBbackups"
+WRK_DIR="/home/frappe/frappe-bench"
+GIT_URL="https://gitlab-ci-token:glpat-yk-_nkFvkGysxbYUevnz@gitlab.caratred.com/ganesh.s/EzyinvoiceDemo.git"
+SITE_NAME="ezyinvoicing.local"
+TAG_PREFIX="stable"
+BRANCH_NAME="Merge_Branches"
 
-# Check if custom_app exists in frappe-bench
-if [ ! -d "/home/frappe/frappe-bench/apps/$custom_app" ]; then
-  # If it doesn't exist, get the app from git_url and install it
+# Create a backup of the app
+# change directory to frappe-bench
+  cd $WORK_DIR
+  echo "Creating backup of $SITE_NAME..."
+  bench --site $SITE_NAME backup --with-files --backup-path $BKP_DIR_TO_COPY
+  echo "Backup created successfully"
+
+# Check if $APP_NAME exists in frappe-bench
+if [ ! -d "$WORK_DIR/apps/$APP_NAME" | echo "$APP_NAME already exists"]; then
+  # If it doesn't exist, get the app from GIT_URL and install it
   # Change to the bench directory 
-  cd /home/frappe/frappe-bench
-  bench get-app $git_url
-  bench --site $site_name install-app $custom_app
+  cd $WRK_DIR
+  echo "cloning $APP_NAME in frappe-bench"
+  bench get-app $GIT_URL
+  echo "Installing $APP_NAME in $SITE_NAME
+  bench --site $SITE_NAME install-app $APP_NAME
+  echo "$APP_NAME installed successfully in $SITE_NAME"
 fi
 
-# Change to the app directory and check for the current branch
-cd /home/frappe/frappe-bench/apps/$custom_app
-current_branch=$(git rev-parse --abbrev-ref HEAD)
+# Create a backup of the app
+# change directory to frappe-bench
+  cd $WORK_DIR
+  echo "Creating backup of $APP_NAME..."
+  bench --site $SITE_NAME backup --with-files --backup-path $BKP_DIR_TO_COPY
+  echo "Backup created successfully"
 
-if [ "$current_branch" == "$branch_name" ]; then
-  # If the current branch is master, fetch the latest tag with the prefix and checkout to it
+# Change to the app directory and check for the current branch
+  cd $APP_NAME
+  CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  COMMIT_ID=$(git rev-parse $CURRENT_BRANCH)
+  echo "Latest commit ID of branch $CURRENT_BRANCH: $COMMIT_ID"
+
+if [ "$CURRENT_BRANCH" == "$BRANCH_NAME" ]; then
+  # If the current branch is $BRANCH_NAME, fetch the latest tag with the prefix and checkout to it
   git fetch --tags
-  latest_tag=$(git describe --tags --match "${tag_prefix}*" `git rev-list --tags --max-count=1`)
-  if [ -n "$latest_tag" ]; then
-    git checkout $latest_tag
+  LATEST_TAG=$(git describe --tags --match "${TAG_PREFIX}*" `git rev-list --tags --max-count=1`)
+  if [ -n "$LATEST_TAG" ]; then
+    git checkout $LATEST_TAG
   else
-    echo "$tag_prefix not found"
+    echo "$TAG_PREFIX not found"
     exit 1
   fi
 else
-  # If the current branch is not master, checkout to it and fetch the latest tag with the prefix
-  git checkout $branch_name
+  # If the current branch is not $CURRENT_BRANCH, checkout to it and fetch the latest tag with the prefix
+  git checkout $BRANCH_NAME
   git fetch --tags
-  latest_tag=$(git describe --tags --match "${tag_prefix}*" `git rev-list --tags --max-count=1`)
-  if [ -n "$latest_tag" ]; then
-    git checkout $latest_tag
+  LATEST_TAG=$(git describe --tags --match "${TAG_PREFIX}*" `git rev-list --tags --max-count=1`)
+  if [ -n "$LATEST_TAG" ]; then
+    git checkout $LATEST_TAG
   else
-    echo "$tag_prefix not found"
+    echo "$TAG_PREFIX not found"
     exit 1
   fi 
 fi
 
 # Migrate the site and set up requirements
-bench --site $site_name migrate
+  bench --site $SITE_NAME migrate
 
 # Check if the migration was successful
 if [ $? -eq 0 ]; then
   bench setup requirements
-  echo "$custom_app updated successfully"
+  echo "$APP_NAME updated successfully"
 else
   # If the migration failed, checkout the previous tag branch
-  git checkout $current_branch
-  bench --site $site_name migrate
+  git checkout $COMMIT_ID
+  bench --site $SITE_NAME migrate
 fi
