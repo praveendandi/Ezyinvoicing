c0888e4a28eb0e3a307fe87f3809b3334dd33851
arvind <arvind@caratred.com>
2023-04-03 06:09:58 -0400

    custom_app_deploy script modified

--- custom_app_deploy.sh
+++ custom_app_deploy.sh
@@ -4,45 +4,51 @@
 APP_NAME="version2_app"
 BKP_DIR="~/DBbackups"
 WORK_DIR="/home/frappe/frappe-bench"
-GIT_CI_TOKEN=glpat-yk-_nkFvkGysxbYUevnz
+GIT_CI_TOKEN="glpat-yk-_nkFvkGysxbYUevnz"
 GIT_URL="https://gitlab-ci-token:"$GIT_CI_TOKEN"@gitlab.caratred.com/ganesh.s/EzyinvoiceDemo.git"
 SITE_NAME="ezyinvoicing.local"
 TAG_PREFIX="stable"
 BRANCH_NAME="master"
 
-# Create a backup of the app
+# Create a backup of the site
 
 # change directory to frappe-bench
-  cd /home/frappe
-  . py37-venv/bin/activate
-  cd $WORK_DIR
-  echo "Creating backup of $SITE_NAME..."
-  bench --site $SITE_NAME backup --with-files --backup-path $BKP_DIR
-  echo "Backup created successfully"
+   cd $WORK_DIR
+if bench --site $SITE_NAME list-apps | grep -q "$APP_NAME"; then
+   echo "$APP_NAME is installed on the site $SITE_NAME"
+   echo "Creating backup..."
+# If custom_app is installed, create a backup of the site
+   bench --site $SITE_NAME backup --backup-path $BKP_DIR
+   echo "Backup created successfully!"
+else
+# If custom_app is not installed, display a message and exit
+   echo "$APP_NAME not found in site. Backup not created."
+   exit 1
+fi
 
-# Check if $APP_NAME exists in frappe-bench and take backup of the site
+# Check if $APP_NAME exists in frappe-bench
 if [ ! -d "$WORK_DIR/apps/$APP_NAME" ]; then
 # If it doesn't exist, get the app from GIT_URL and install it
-  echo "$APP_NAME doesn't exists"
+   echo "$APP_NAME doesn't exists"
 # Change to the bench directory 
-  cd $WORK_DIR
-  echo "cloning $APP_NAME in frappe-bench"
-  bench get-app $GIT_URL --branch $BRANCH_NAME
-  echo "Installing $APP_NAME in $SITE_NAME
-  bench --site $SITE_NAME install-app $APP_NAME
-  echo "$APP_NAME installed successfully in $SITE_NAME"
+   cd $WORK_DIR
+   echo "cloning $APP_NAME in frappe-bench"
+   bench get-app $GIT_URL --branch $BRANCH_NAME
+   echo "Installing $APP_NAME in $SITE_NAME
+   bench --site $SITE_NAME install-app $APP_NAME
+   echo "$APP_NAME installed successfully in $SITE_NAME"
 fi
 
 # Change to the app directory and check for the current branch
-  cd $WORK_DIR/$APP_NAME
-  CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-  COMMIT_ID=$(git rev-parse --short $CURRENT_BRANCH)
-  echo "Latest commit ID of branch $CURRENT_BRANCH: $COMMIT_ID"
+   cd $WORK_DIR/$APP_NAME
+   CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+   COMMIT_ID=$(git rev-parse --short $CURRENT_BRANCH)
+   echo "Latest commit ID of branch $CURRENT_BRANCH: $COMMIT_ID"
 
 if [ "$CURRENT_BRANCH" == "$BRANCH_NAME" ]; then
-  # If the current branch is $BRANCH_NAME, fetch the latest tag with the prefix and checkout to it
-  git fetch --tags
-  LATEST_TAG=$(git describe --tags --match "${TAG_PREFIX}*" `git rev-list --tags --max-count=1`)
+# If the current branch is $BRANCH_NAME, fetch the latest tag with the prefix and checkout to it
+   git fetch --tags
+   LATEST_TAG=$(git describe --tags --match "${TAG_PREFIX}*" `git rev-list --tags --max-count=1`)
   if [ -n "$LATEST_TAG" ]; then
     git checkout $LATEST_TAG
   else
@@ -74,5 +80,4 @@ if [ $? -eq 0 ]; then
     git checkout $COMMIT_ID
     bench --site $SITE_NAME migrate
     bench version
-    exit 1
 fi
