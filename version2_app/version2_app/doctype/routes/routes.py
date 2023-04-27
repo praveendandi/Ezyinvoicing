581b0082d10bfced6e220e127c221bdf9544d469
Mounika <mounika.b@caratred.com>
2023-04-27 17:37:02 +0530

    role and permssions

--- version2_app/version2_app/doctype/routes/routes.py
+++ version2_app/version2_app/doctype/routes/routes.py
@@ -44,7 +44,8 @@ def reset_initial_password(user):
         if get_user_details[0]['last_active'] == None and get_user_details[0]['last_password_reset_date'] == None:
             return {'user': get_user_details[0]['email'], 'success': True, "message": "New login force to reset"}
         else:
-            return {'user': get_user_details[0]['email'], 'success': False, "message": "Old login"}
+            if get_user_details[0]['last_active'] != None or get_user_details[0]['last_password_reset_date'] != None:
+                return {'user': get_user_details[0]['email'], 'success': False, "message": "Old login"}
             # if get_user_details[0]['last_active'] != None or get_user_details[0]['last_password_reset_date'] != None:
                 # last_password_reset_date = frappe.db.get_list('User',filters={'username':user},fields=['last_password_reset_date'], ignore_permissions=True)
                 # date_obj = last_password_reset_date[0]['last_password_reset_date']
