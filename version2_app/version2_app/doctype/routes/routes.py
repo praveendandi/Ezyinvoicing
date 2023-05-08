967065c4e2a31ed344aae953b8e82b812750d098
Mounika <mounika.b@caratred.com>
2023-05-08 10:03:11 +0530

    changed routes path

--- version2_app/version2_app/doctype/routes/routes.py
+++ version2_app/version2_app/doctype/routes/routes.py
@@ -9,6 +9,7 @@ import datetime
 import sys, traceback
 # from frappe import _
 import frappe
+import pandas as pd
 from frappe.utils import now_datetime, add_days
 
 
@@ -100,3 +101,19 @@ def disable_inactive_users():
             frappe.db.set_value('User', user.name, 'enabled', 0)
             frappe.db.commit()    
         return user
+    
+
+
+
+ 
+@frappe.whitelist()
+def migrating_routes():
+    folder_path = frappe.utils.get_bench_path()
+    file_path = folder_path +"/apps/version2_app/version2_app/EzyInvRoutes.xlsx"
+    dataframe1 = pd.read_excel(file_path)
+    data = dataframe1.to_dict("records")
+    for i in data:
+        if not frappe.db.exists("Routes",{"route_name":i['Route Name']}):
+            routes_data= frappe.get_doc({"doctype":"Routes","route_name":i['Route Name'],"route":i['Route'],"module":i['Module']})
+            routes_data.insert()
+                
