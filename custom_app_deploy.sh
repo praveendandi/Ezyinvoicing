cb59ebee25338beaa4a023dbbf94a203d6a7c97c
arvind <arvind@caratred.com>
2023-05-17 09:08:46 -0400

    changes in updated script

--- custom_app_deploy.sh
+++ custom_app_deploy.sh
@@ -4,53 +4,52 @@
 APP_NAME=version2_app
 BKP_DIR=/home/frappe/DBbackups
 WORK_DIR=/home/frappe/HIEX-bench
-PARSERS-DIR=$WORK_DIR/apps/$APP_NAME/$APP_NAME/parsers_invoice/invoice_parsers/
+INVOICE_PARSERS=/home/frappe/HIEX-bench/apps/version2_app/version2_app/parsers_invoice/invoice_parsers
 GIT_CI_TOKEN=glpat-yk-_nkFvkGysxbYUevnz
 GIT_URL=https://gitlab-ci-token:$GIT_CI_TOKEN@gitlab.caratred.com/ganesh.s/EzyinvoiceDemo.git
 SITE_NAME=ezyinvoicing.local
 TAG_PREFIX=stable
 BRANCH_NAME=Merge_Branches
-timestamp=$(date +%Y%m%d%H%M%S)
 
 # Create a backup of the site
 
 # Change directory to frappe-bench
-   cd $WORK_DIR || exit
+cd $WORK_DIR || exit
 if bench --site $SITE_NAME list-apps | grep -q "$APP_NAME"; then
-   echo "$APP_NAME is installed on the site $SITE_NAME"
-   echo "Creating backup..."
+  echo "$APP_NAME is installed on the site $SITE_NAME"
+  echo "Creating backup..."
 # If custom_app is installed, create a backup of the site
-   bench --site $SITE_NAME backup --compress --backup-path "$BKP_DIR"
-   echo "Backup created successfully!"
+  bench --site $SITE_NAME backup --compress --backup-path "$BKP_DIR"
+  echo "Backup created successfully!"
 else
 # If custom_app is not installed, display a message and exit
-   echo $APP_NAME not found in site. Backup not created.
-   exit 1
+  echo $APP_NAME not found in site. Backup not created.
+  exit 1
 fi
 
 # Check if $APP_NAME exists in frappe-bench
 if [ ! -d "$WORK_DIR/apps/$APP_NAME" ]; then
 # If it doesn't exist, get the app from GIT_URL and install it
-   echo "$APP_NAME doesn't exists"
+  echo "$APP_NAME doesn't exists"
 # Change to the bench directory
-   cd "$WORK_DIR || exit" || exit
-   echo cloning $APP_NAME in frappe-bench
-   bench get-app $GIT_URL --branch $BRANCH_NAME
-   echo Installing $APP_NAME in $SITE_NAME
-   bench --site $SITE_NAME install-app $APP_NAME
-   echo $APP_NAME installed successfully in $SITE_NAME
+  cd "$WORK_DIR || exit" || exit
+  echo cloning $APP_NAME in frappe-bench
+  bench get-app $GIT_URL --branch $BRANCH_NAME
+  echo Installing $APP_NAME in $SITE_NAME
+  bench --site $SITE_NAME install-app $APP_NAME
+  echo $APP_NAME installed successfully in $SITE_NAME
 fi
 
 # Change to the app directory and check for the current branch
-   cd "$WORK_DIR"/apps/$APP_NAME || exit
-   CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-   COMMIT_ID=$(git rev-parse --short "$CURRENT_BRANCH")
-   echo Latest commit ID of branch "$CURRENT_BRANCH": "$COMMIT_ID"
+  cd "$WORK_DIR"/apps/$APP_NAME || exit
+  CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  COMMIT_ID=$(git rev-parse --short "$CURRENT_BRANCH")
+  echo Latest commit ID of branch "$CURRENT_BRANCH": "$COMMIT_ID"
 
 if [ "$CURRENT_BRANCH" == "$BRANCH_NAME" ]; then
 # If the current branch is $BRANCH_NAME, fetch the latest tag with the prefix and checkout to it
-   git fetch --tags --all
-   LATEST_TAG=$(git describe --tags --match "${TAG_PREFIX}-*" "$(git rev-list --tags --max-count=1)")
+  git fetch --tags --all
+  LATEST_TAG=$(git describe --tags --match "${TAG_PREFIX}-*" "$(git rev-list --tags --max-count=1)")
   if [ -n "$LATEST_TAG" ]; then
     git checkout "$LATEST_TAG"
   else
@@ -69,17 +68,19 @@ else
     exit 1
   fi
 fi
-
 # Migrate the site and set up requirements
-  bench --site $SITE_NAME migrate
+#  bench --site $SITE_NAME migrate
   bench setup requirements
 # Check if the migration was successful
-if [! "bench --site $SITE_NAME migrate"]; then
+if ! "bench --site $SITE_NAME migrate"; then
     echo $APP_NAME updated successfully
+    echo updating invoice-parsers 
+    cd $INVOICE_PARSERS || exit
+    git pull origin master
     exit 1
   else
 # If the migration failed, checkout the previous tag commit id branch
+    cd $WORK_DIR/apps/$APP_NAME || exit
     git checkout "$COMMIT_ID"
     bench --site $SITE_NAME migrate
-    bench version
 fi
